49dc23261ccc1da8105fbd8f6309652b0ecc4a27
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-01-13 14:15:56 +0530

    Time: 10 ms (65.05%), Space: 16.2 MB (98.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,16 +1,46 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& v) {
-        //1 3 2 4 5 : n=5
+       
         int n=v.size();
-        map<int,int>mp;
+        
+       //there can be at max 2 such elements.
+        
+        //following algo finds most 2 frequent numbers in array.
+        int cand1,cnt1=0;
+        int cand2,cnt2=0;
         for(int i=0;i<n;i++){
-            mp[v[i]]++;
+            //cand1 ,cand2 do not compete with each other consider them fighting elections for different seats.
+            if(cnt1==0&&v[i]!=cand2){
+                cand1=v[i];
+                cnt1=1;
+               
+            }
+            else if(cnt2==0&&v[i]!=cand1){
+                cand2=v[i];
+                cnt2=1;
+            }
+            else if(v[i]==cand1){
+               cnt1++;
+            }
+            else if(v[i]==cand2){
+                cnt2++;
+            }
+            else{
+                cnt1--,cnt2--;
+            }
         }
+        
+        //check freq of these 2 elements which are potential candidates
+        int freq1=0,freq2=0;
+        for(auto&x:v){
+            if(x==cand1)freq1++;
+            else if(x==cand2)freq2++;
+        }
+        
         vector<int>ans;
-        for(auto&p:mp){
-            if(p.second>n/3)ans.push_back(p.first);
-        }
+        if(freq1>n/3)ans.push_back(cand1);
+        if(freq2>n/3)ans.push_back(cand2);
         return ans;
     }
 };
