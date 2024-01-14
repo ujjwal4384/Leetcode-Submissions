d5b6d17e1cf3a3c4535f5646ecf5341175c57200
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-01-15 00:44:34 +0530

    Time: 19 ms (90.03%), Space: 89.6 MB (70.48%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -8,26 +8,28 @@ public:
         int nA = A.size(), nB = B.size();
         int l = 0, r = nA;
         int left= (nA + nB + 1) / 2 ;  
+         int maxLeftA,minRightA,maxLeftB,minRightB;
         while (l <= r) {
             int cutA = (l + r) >> 1;
             int cutB = left - cutA;
             
-            int maxLeftA = (cutA == 0) ? INT_MIN : A[cutA - 1];
-            int minRightA = (cutA == nA) ? INT_MAX : A[cutA];
-            int maxLeftB = (cutB == 0) ? INT_MIN : B[cutB - 1];
-            int minRightB = (cutB == nB) ? INT_MAX : B[cutB];
+             maxLeftA = (cutA == 0) ? INT_MIN : A[cutA - 1];
+             minRightA = (cutA == nA) ? INT_MAX : A[cutA];
+             maxLeftB = (cutB == 0) ? INT_MIN : B[cutB - 1];
+             minRightB = (cutB == nB) ? INT_MAX : B[cutB];
             
             if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
-                if ((nA + nB) % 2 == 0) return 
-                (max(maxLeftA, maxLeftB)+min(minRightA,minRightB))/2.0;
-                else return max(maxLeftA, maxLeftB); 
+             break;  
             } 
             else if (maxLeftA > minRightB) 
                 r = cutA - 1;
             else 
                 l = cutA + 1;
         }
-        return 0.0;
+          if ((nA + nB) % 2 == 0) return 
+                (max(maxLeftA, maxLeftB)+min(minRightA,minRightB))/2.0;
+                else return max(maxLeftA, maxLeftB); 
+       
     }
     
 };
