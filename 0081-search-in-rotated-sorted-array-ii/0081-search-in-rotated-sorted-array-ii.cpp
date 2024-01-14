e70bbcc19280682f097bf34bdda28fe4b700e2b9
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-01-14 19:00:23 +0530

    Time: 3 ms (85.44%), Space: 14.4 MB (23.92%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        //find pivot
-        //2 increasing sequences. b.s
-         int pi=0;
-        int n= nums.size();
-        for(int i=1;i<n;i++){
-            if(nums[i]<nums[i-1]){
-                pi=i-1;
-                break;
-            }
-        }
-        
-        return bs(0, pi, nums, target) || bs(pi+1, n-1, nums, target);
-    }
-    
-    bool bs(int l, int r, vector<int>&v, int& target){
-       if(l<=r){ 
-        int m = (l+r)>>1;
-        if(v[m]==target)return true;
-        else if(v[m]<target){
-            return bs(m+1, r, v, target);
-        }else{
-            return bs(l, m-1, v, target);
-        }
+       int n=nums.size();
+       int low=0,high=n-1;
+       while(low<=high){
+           int mid=(low+high)>>1;
+           if(nums[mid]==target)return true;
+           //this is the only edge case which come on duplicates
+           else if(nums[low]==nums[mid] & nums[mid]==nums[high]){
+             low++,high--;
+           }
+
+           else{
+               if(nums[low]<=nums[mid]){
+                    if(nums[low]<=target && target<=nums[mid]){
+                        high=mid-1;
+                    }else{
+                        low=mid+1;
+                    }
+               }else{
+                   if(nums[mid]<=target && target<=nums[high]){
+                        low=mid+1;
+                    }else{
+                        high=mid-1;
+                    }
+               }
+           }
        }
-       return false;     
+
+      return false; 
     }
 };
