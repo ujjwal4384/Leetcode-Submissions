fc8c325ce8bea3eef2145c5711b29243ea6c4787
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-01-15 00:41:09 +0530

    Time: 27 ms (59.76%), Space: 89.6 MB (70.48%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
-        int n1=v1.size();
-        int n2=v2.size();
-        if(n1<n2)return findMedianSortedArrays(v2,v1);
-        int n=n1+n2;
-        //v1=[1,4,5] 
-        //v2=[2,2,3,4,6]
-        //make total elements on left to be (n+1)/2;
-        int left =(n+1)/2;
-        int low1=max(0, left-n2),high1=min(n1,left);
-        int l1,l2,r1,r2;
-        while(low1<=high1){
-            int mid1= (low1+high1)>>1;
-            int mid2= left-mid1;
-          
-            l1= mid1-1>=0? v1[mid1-1] : INT_MIN;
-            l2=mid2-1>=0?v2[mid2-1]:INT_MIN;
-            r1=mid1<n1?v1[mid1]:INT_MAX;
-            r2=mid2<n2?v2[mid2]:INT_MAX;
-                
-            if(l1<=r2 && l2<=r1){
-                break;
-            }
-            else if(l1>r2){
-                high1=mid1-1;
-            }else{
-                low1=mid1+1;
-            }
-                
+     double findMedianSortedArrays(vector<int>& A, vector<int>& B) 
+    {    
+        if (A.size() > B.size()) 
+            return findMedianSortedArrays(B, A);
+        
+        int nA = A.size(), nB = B.size();
+        int l = 0, r = nA;
+        
+        while (l <= r) {
+            int cutA = (l + r) / 2;
+            int cutB = (nA + nB + 1) / 2 - cutA;
+            
+            int maxLeftA = (cutA == 0) ? INT_MIN : A[cutA - 1];
+            int minRightA = (cutA == nA) ? INT_MAX : A[cutA];
+            int maxLeftB = (cutB == 0) ? INT_MIN : B[cutB - 1];
+            int minRightB = (cutB == nB) ? INT_MAX : B[cutB];
+            
+            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if ((nA + nB) % 2 == 0) return 
+                (max(maxLeftA, maxLeftB)+min(minRightA,minRightB))/2.0;
+                else return max(maxLeftA, maxLeftB); 
+            } 
+            else if (maxLeftA > minRightB) 
+                r = cutA - 1;
+            else 
+                l = cutA + 1;
         }
-        cout<<l1<<","<<l2;
-        return n%2? max(l1,l2) : (max(l1,l2)+min(r1,r2))/2.0; 
+        return 0.0;
     }
+    
 };
