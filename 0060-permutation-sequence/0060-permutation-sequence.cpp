5fca33d48a7acd3b7670bd5ca099fbfe997e3f36
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-01-20 01:11:23 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (9.63%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-   int fact(int n){
-        if(n<=1) return 1;
-        return n * fact(n-1);
-   } 
-   void f(int n, int k, string&ans, set<int>&s){
+   // int fact(int n){
+   //      if(n<=1) return 1;
+   //      return n * fact(n-1);
+   // } 
+   void f(int n, int k, string&ans, set<int>&s, int fact[]){
        if(n==0){
            return ;
        }  
-         int times = fact(n-1);
+         int times = fact[n-1];
          int move = k/times ;
          int newK = k%times ; 
        
@@ -17,15 +17,20 @@ public:
          ans+= to_string(*it);
          s.erase(it);
        
-         f(n-1, newK, ans, s);
+         f(n-1, newK, ans, s, fact);
     }
     
     string getPermutation(int n, int k) {
+        int fact[10]={};
+        fact[0] = 1,fact[1]=1;
+        for(int k=2;k<=9;k++){
+            fact[k]=k * fact[k-1];
+        }
         k--;
         set<int>s;
         for(int k=1;k<=n;k++)s.insert(k);
         string ans="";
-        f(n,k,ans, s);
+        f(n,k,ans, s, fact);
        return ans; 
     }
 };
