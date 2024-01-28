7e0ed3fe5e48c2a9108de8f5ad0d67aca6c92ebc
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-01-29 00:38:11 +0530

    Time: 513 ms (21.01%), Space: 214.4 MB (6.29%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,41 +3,39 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
      //standard problem handled by 2 multisets/priority queue.
         int n=nums.size();
-        multiset<int>maxi,rem;
+        multiset<int>rem;
+        int maxi=INT_MIN;
         for(int i=0;i<k;i++){
             rem.insert(nums[i]);
         }
         vector<int>res;
-        maxi.insert((*--rem.end()));
+        maxi= *(--rem.end());
         rem.erase((--rem.end()));
-        res.push_back(*maxi.begin());
+        res.push_back(maxi);
         for(int i=k;i<n;i++){
             int newX = nums[i];
             int removeX = nums[i-k];
             
             //removal
-            if(maxi.find(removeX)!=maxi.end()){
-                auto it = maxi.find(removeX);
-                maxi.erase(it);
+            if(removeX==maxi){
+                maxi = INT_MIN;
             }else{
                 auto it = rem.find(removeX);
                 rem.erase(it);
             } 
             //addition
-            if(!maxi.empty() && newX> *(--maxi.end())){
-                maxi.insert(newX);
+            if(maxi!=INT_MIN && newX> maxi){
+                rem.insert(maxi);
+                maxi = newX;
             }else{
                 rem.insert(newX);
             }
             //balancing
-            if(maxi.size()>1){
-                rem.insert(*maxi.begin());
-                maxi.erase(maxi.begin());
-            }else if(maxi.empty()){
-                maxi.insert(*(--rem.end()));
+            if(maxi==INT_MIN){
+                maxi=(*(--rem.end()));
                 rem.erase(--rem.end());
             }
-            res.push_back(*maxi.begin());
+            res.push_back(maxi);
         }
        return res; 
     }
