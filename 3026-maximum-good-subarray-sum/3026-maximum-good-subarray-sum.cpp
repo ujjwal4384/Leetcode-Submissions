ff5dddd0921e3782895db2c2939b0e4f136c70c0
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-02-03 22:39:54 +0530

    Time: 1492 ms (20.00%), Space: 182.9 MB (40.00%) - LeetHub

--- 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
+++ 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
@@ -2,37 +2,52 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<long long int>pre(n,0LL);
+        vector<long long>pre(n,0);
         for(int i=0;i<n;i++){
             if(i==0)pre[i]=nums[i];
             else pre[i]=pre[i-1]+nums[i];
         }
-
-        
-        unordered_map<int, vector<int>>mp;
-        
-        long long int maxi= -1e17;
-        long long int s=0;
-
+       
+      
+        map<int, vector<int>>mp;
+        long long maxi=-1e17;
+        long long s=0;
+        bool flag=false;
         for(int i=0;i<n;i++){
           
             s+=nums[i];
-          
-            for(auto x : {nums[i]-k, nums[i]+k}){
-                     for(auto & id: mp[x]){
-                        long long cur = s- (id==0?0:pre[id-1]);
-                         maxi = max(cur, maxi); 
-                     }
+            int a=nums[i]-k,b=nums[i]+k;
+           
+            if(mp.find(a)!=mp.end()){
+                 auto it=mp.find(a);
+                
+                 for(auto & id: it->second){
+                     flag=true;
+                    long long cur = s- (id==0?0:pre[id-1]);
+                    if(cur>maxi) maxi = cur; 
+                   
+                 }
+                
             }
-            
+            if(mp.find(b)!=mp.end()){
+                
+                 auto it=mp.find(b);
+               
+               
+                 for(auto & id: it->second){
+                       flag=true;
+                    long long cur =s- (id==0?0:pre[id-1]);
+                    if(cur>maxi) maxi = cur; 
+                 }
+               
+                
+            }
             mp[nums[i]].push_back(i);
-        
-        }
              
+        }
+        if(!flag) return 0;
         
-       
-        
-        return maxi== -1e17 ? 0 : maxi;
+        return maxi;
         
     }
 };
