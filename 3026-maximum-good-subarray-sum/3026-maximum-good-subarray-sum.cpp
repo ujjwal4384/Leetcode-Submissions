3aa115110769f3fa05356314af40070a4077d5aa
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-02-03 23:58:07 +0530

    Time: 278 ms (100.00%), Space: 148.5 MB (40.00%) - LeetHub

--- 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
+++ 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         long long res = LLONG_MIN;
-        vector<long long> psum{0};
-        unordered_map<int, int> m;
-    for (int i = 0; i < nums.size(); ++i) {
-        psum.push_back(psum.back() + nums[i]); // Prefix Sum
-        for(auto x:{nums[i]-k, nums[i]+k}){   
-                auto it =m.find(x);
-                if (it != m.end()){
-                    long long cur = psum[i + 1] - psum[it->second];
-                    res = max(res, cur); // n - k
-                }
-        }    
-        auto it=m.find(nums[i]);
-        if(it==m.end()){
-             m[nums[i]] = i; 
-             continue;
+         int n=nums.size();
+        vector<long long>pre(n,0);
+        for(int i=0;i<n;i++){
+            if(i==0)pre[i]=nums[i];
+            else pre[i]=pre[i-1]+nums[i];
         }
-        long long tempSum = psum[i] - psum[it->second] ;
-        if(tempSum<=0){
-             m[nums[i]] = i; 
-             continue;
+       
+        unordered_map<long long, long long> m;
+    for (int i = 0; i < nums.size(); ++i) {
+        if (auto it = m.find(nums[i] - k); it != end(m)){
+            res = max(res, pre[i] - (it->second==0?0:pre[it->second-1]) ); // n - k
+        }
+        if (auto it = m.find(nums[i] + k); it != end(m)){
+             res = max(res, pre[i] - (it->second==0?0:pre[it->second-1]) ); // n + k
+        }
+        auto it =m.find(nums[i]);
+        if(it==m.end()){
+             m[nums[i]] = i;
+            continue;
+        }
+        long long int tempSum =(i==0?0:pre[i-1]) - (it->second==0?0LL:pre[it->second-1]);
+        if (tempSum<=0){
+            m[nums[i]] = i; // Kadane
         }
     }
-        
     return res == LLONG_MIN ? 0 : res;
     }
 };
