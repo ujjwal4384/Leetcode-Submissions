1a4f3a528f2804c822daa82162865539c8aadfaf
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-02-03 22:18:44 +0530

    Time: 1458 ms (20.00%), Space: 245.3 MB (40.00%) - LeetHub

--- 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
+++ 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int, vector<int>> lookup;
-        vector<long long int> prefix(n, 0);
-        
-        for (int i = 0; i < n; i++){
-           
-            prefix[i] = nums[i];
-            if (i > 0){
-                prefix[i] += prefix[i-1];
-            }
+        int n=nums.size();
+        vector<long long int>pre(n,0LL);
+        for(int i=0;i<n;i++){
+            if(i==0)pre[i]=nums[i];
+            else pre[i]=pre[i-1]+nums[i];
         }
 
         
-        bool found = false;
-        long long int ans{0};
-        long long cur=0;
-        for (int i = 0; i < n; i++){
-            cur+=nums[i];
-            for (int a : {nums[i] - k, nums[i] + k}){
-                for (int j : lookup[a]){
-                    
-                        long long int s;
-                       
-                        s = cur - (j-1>=0?prefix[j-1]:0LL);
-                        
-                    if (!found){
-                            ans = s;
-                            found = true;
-                        } else {
-                            ans = std::max(ans, s);
-                        }
-                    
-                }                
-            }
-             lookup[nums[i]].push_back(i);
-        }
+        unordered_map<int, vector<int>>mp;
         
-        return ans;
+        long long int maxi= 0;
+        long long int s=0;
+        bool flag=false;
+        for(int i=0;i<n;i++){
+          
+            s+=nums[i];
+          
+            for(auto x : {nums[i]-k, nums[i]+k}){
+                for(auto id:mp[x]){
+                            long long int cur = s- (id==0?0:pre[id-1]);
+                             if(!flag)maxi=cur;
+                             else maxi = max(cur, maxi); 
+                             flag=true;
+                }
+            }
+            
+            mp[nums[i]].push_back(i);
+             
+        }
+        if(!flag) return 0;
+        
+        return maxi;
         
     }
 };
