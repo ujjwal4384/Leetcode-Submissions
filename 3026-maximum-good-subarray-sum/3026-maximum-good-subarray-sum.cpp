f8b7056466be1d39611c2d31176e59b6479b1195
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-02-03 23:27:47 +0530

    Time: 261 ms (100.00%), Space: 151.4 MB (40.00%) - LeetHub

--- 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
+++ 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         long long res = LLONG_MIN;
-    vector<long long> psum{0};
-    unordered_map<int, int> m;
+        vector<long long> psum{0};
+        unordered_map<int, int> m;
     for (int i = 0; i < nums.size(); ++i) {
         psum.push_back(psum.back() + nums[i]); // Prefix Sum
         if (auto it = m.find(nums[i] - k); it != end(m))
