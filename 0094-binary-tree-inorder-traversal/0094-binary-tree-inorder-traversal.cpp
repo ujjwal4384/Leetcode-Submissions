41dc162f62ed548a76eed0d128516b08e29ef5df
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-02-14 01:29:50 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (15.19%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -17,9 +17,60 @@ public:
         v.push_back(root->val);
         f(root->right, v);
     }
-    vector<int> inorderTraversal(TreeNode* root) {
-         vector<int>v;
-        f(root, v);
+    
+    vector<int> stackFunc(TreeNode* root){
+           vector<int>v;
+       
+        stack<TreeNode*>st;
+       
+        TreeNode* cur=root;
+        while(cur || !st.empty()){
+           while(cur){
+               st.push(cur);
+               cur=cur->left;
+           }
+            TreeNode* node = st.top();              
+            v.push_back(node->val);
+            st.pop();
+            cur=node->right;
+          
+        }
         return v;
+    }
+    void morrisInorder(TreeNode* root, vector<int>&inorder){
+        TreeNode* cur=root;
+        while(cur){
+//case 1: no left subtree: just pick the node and move right;
+            if(!cur->left){
+                inorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            
+            else{
+                TreeNode* prev = cur->left;
+                TreeNode* temp = prev;      
+                while(temp->right && temp->right != cur){
+                    temp=temp->right;
+                }
+//case2: rightmost node of left subtree points to null. connect it to cur node.
+                if(!temp->right){
+                    temp->right = cur;
+                    cur= cur->left;
+                }
+ //case3: rightmost node of left subtree already pointing to curren node=> break the links, pick this node and move right.                 
+                else {
+                    temp->right = NULL;
+                    inorder.push_back(cur->val);
+                    cur=cur->right;
+                }
+            }
+        }
+         
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+      vector<int>inorder;
+        //morris
+        morrisInorder(root, inorder);
+        return inorder;
     }
 };
