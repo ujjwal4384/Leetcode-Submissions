6dccac54412944ead8e64cfb4b1236861ac8240a
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-02-13 23:34:31 +0530

    Time: 6 ms (95.02%), Space: 26.2 MB (54.14%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,26 +11,23 @@
  */
 class Solution {
 public:
-    TreeNode* f(vector<int>& preorder, vector<int>& inorder, int pre_l, int pre_r, int in_l, int in_r){
+    TreeNode* f(vector<int>& preorder, vector<int>& inorder, int pre_l, int pre_r, int in_l, int in_r, unordered_map<int,int>&mp){
         if(pre_l> pre_r || in_l > in_r) return NULL;
         
         TreeNode* node = new TreeNode(preorder[pre_l]);
         
         //find node's index in inorder
-        int node_id = -1;
-        for(int i=in_l;i<=in_r; i++){
-            if(inorder[i] == preorder[pre_l]){
-                node_id = i; break;
-            }
-        }
+        int node_id = mp[preorder[pre_l]];
         int cnt_left = node_id- in_l;
         //now make node->left and right subtree
-        node->left = f(preorder, inorder, pre_l+1, pre_l+cnt_left, in_l, node_id-1);
-        node->right = f(preorder, inorder, pre_l+cnt_left+1, pre_r, node_id+1, in_r);
+        node->left = f(preorder, inorder, pre_l+1, pre_l+cnt_left, in_l, node_id-1, mp);
+        node->right = f(preorder, inorder, pre_l+cnt_left+1, pre_r, node_id+1, in_r, mp);
         
      return node;   
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return f(preorder, inorder,0, preorder.size() -1, 0, inorder.size()-1);
+        unordered_map<int, int>mp;
+        for(int i=0;i<inorder.size(); i++)mp[inorder[i]] = i;
+        return f(preorder, inorder,0, preorder.size() -1, 0, inorder.size()-1, mp);
     }
 };
