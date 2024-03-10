889698b7c5edf82c6baf6782459202a4fe807e56
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-03-10 15:33:01 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (59.29%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -13,11 +13,21 @@ public:
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int row=triangle.size();
-        int ans=INT_MAX;
+      
         int dp[201][201];
-        memset(dp,-1,sizeof(dp));
-        for(int col=0;col<row;col++){
-           ans= min(ans, f(row-1, col, triangle,dp));
+        memset(dp,0,sizeof(dp));
+        dp[0][0] = triangle[0][0];
+        for(int i=1;i<row;i++){
+            for(int j=0;j<=i;j++){
+                int way1 = INT_MAX,way2=INT_MAX;
+                if(j<=i-1)way1=dp[i-1][j];
+                if(j-1>=0)way2 = dp[i-1][j-1];
+                dp[i][j] = min(way1, way2) + triangle[i][j];
+            }
+        }
+        int ans=INT_MAX;
+        for(int j=0;j<triangle.size();j++){
+            ans= min(ans, dp[triangle.size()-1][j]);
         }
        return ans; 
     }
