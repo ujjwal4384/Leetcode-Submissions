cfca9941093655c4a236293d108d80ea867c2662
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-03-17 19:39:45 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (8.56%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -10,7 +10,7 @@ public:
         
         return dp[i][j]= f(i-1, j, obstacleGrid, dp)+ f(i, j-1, obstacleGrid, dp);
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int uniquePathsWithObstaclesMemo(vector<vector<int>>& obstacleGrid) {
         int m= obstacleGrid.size();
         int n= obstacleGrid[0].size();
         if(obstacleGrid[0][0] || obstacleGrid[m-1][n-1]) return 0;
@@ -18,5 +18,25 @@ public:
         int dp[101][101];
         memset(dp, -1, sizeof(dp));
         return f(m-1, n-1, obstacleGrid, dp);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m= obstacleGrid.size();
+        int n= obstacleGrid[0].size();
+        if(obstacleGrid[0][0] || obstacleGrid[m-1][n-1]) return 0;
+        if(m==1 && n==1) return 1;
+        int dp[101][101];
+        memset(dp, 0, sizeof(dp));
+        dp[1][1] =1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                 if(i==1 && j==1)continue;
+                 if(obstacleGrid[i-1][j-1]){
+                     dp[i][j] = 0;
+                     continue;
+                 }
+                 dp[i][j]= dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m][n];
     }
 };
