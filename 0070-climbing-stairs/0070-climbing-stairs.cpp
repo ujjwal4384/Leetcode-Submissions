2a167151867493ce1454f474260be434592c9bc4
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-03-17 19:47:02 +0530

    Time: 3 ms (17.85%), Space: 7.1 MB (34.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        //fibonacci series. log(n)=> matrix exponentiaiton se.
+       int last_second_step=1,last_step=1;
+        if(n==1) return 1;
+        int cur=0;
         
-        int dp[47];
-        memset(dp,0, sizeof(dp));
-        dp[0] = 1;
-        dp[1] = 1;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            cur = last_step + last_second_step;
+            last_second_step = last_step;
+            last_step = cur;
         }
-        return dp[n];
+        return cur;
     }
 };
