296731f0c64ef87d3db584218d51d0183b1573a0
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-03-24 17:59:25 +0530

    Time: 17 ms (49.51%), Space: 17.6 MB (50.87%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void topoSort(int n, vector<vector<int>>& v, vector<int>&order){
+        vector<vector<int>>adj(n);
+        for(int i=0;i<v.size();i++){
+            int x = v[i][0],y=v[i][1];  
+            adj[y].push_back(x);
+        }
+       
+        queue<int>q;
+        map<int,int>indegree;
+        for(int i=0;i<n;i++){
+            for(auto&x:adj[i]){
+                 indegree[x]++;
+            }
+        }
+        vector<bool> vis(2001, false);
+    
+        for(int node=0;node<n;node++){
+            if(indegree[node]==0){
+                q.push(node);
+                vis[node]= true;
+            }
+        }
+       
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                int node = q.front();
+                order.push_back(node);
+                q.pop();
+                for(auto&x:adj[node]){
+                    indegree[x]--;
+                    if(indegree[x]==0 && !vis[x]){
+                        vis[x]=true;
+                        q.push(x);
+                    }
+                }
+            }
+        }
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& v) {
+      vector<int>order;
+       topoSort(numCourses, v, order);
+      
+        if(order.size()!=numCourses) return {};
+        return order;
+        
+    }
+};
