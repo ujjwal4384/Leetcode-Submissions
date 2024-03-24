5580aa7d78c71a97b62c9c9d45a3aa43b6b5734c
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-03-24 13:46:16 +0530

    Time: 4 ms (62.23%), Space: 16.2 MB (23.10%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int adj[4][2] = {{-1,0},{0,-1},{1,0},{0,1}};
+    
+    bool isValid(int i, int j, int m, int n){
+        return i>=0 && j>=0 && i<m && j<n;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        //0= empty cell, 1 = fresh, 2-> rotten orange
+        vector<vector<bool>>vis(m, vector<bool>(n, false));
+        int cnt_fresh=0;
+        queue<pair<int,int>>q;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({i, j});
+                    vis[i][j]= true;
+                }
+                else if(grid[i][j]==1)cnt_fresh++;
+            }
+        }
+        if(cnt_fresh == 0) return 0;
+       
+        if(q.empty()) return -1;
+        
+      // cout<<"HI\n";
+        int time=0;
+        while(!q.empty()){
+            int sz = q.size();
+            bool add=0;
+            while(sz--){
+                auto p = q.front();
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int nx = p.first+ adj[i][0];
+                    int ny = p.second + adj[i][1];
+                    if(isValid(nx,ny,m,n) && !vis[nx][ny] && grid[nx][ny]==1){
+                        add=1;
+                        // cout<<nx<<","<<ny<<endl;
+                        cnt_fresh--;
+                        vis[nx][ny]=true;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+            if(add)time++;
+        }
+       return cnt_fresh>0 ? -1: time; 
+    }
+};
