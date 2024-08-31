b4ce025121c180a55649a3101481888427ad81a7
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-08-31 23:22:09 +0530

    Time: 3 ms (75.74%), Space: 12.7 MB (96.22%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    void f(int pos,vector<int>& candidates,vector<vector<int>>&res, vector<int>&v, int& rem, map<int,bool>&mp){
-        if(rem==0){
-            res.push_back(v);
+void func(int i, vector<int>&v,vector<vector<int>>&vv,vector<int>& arr, int& target, int sum){
+        if(sum>target) return;
+        else if(sum == target){
+            vv.push_back(v);
+            return;
+        } else if(i>=arr.size()){
             return;
         }
-        else if(rem<0 || pos==candidates.size()){
-            return ;
-        }
-        
-      
-            v.push_back(candidates[pos]);
-          
-             rem-= candidates[pos];
-        
-            f(pos+1, candidates, res, v, rem, mp );
-        rem+=candidates[pos];
-         v.pop_back();
-         int k=pos+1;
-         while(k<candidates.size() && candidates[k]==candidates[pos])k++;
-      
-        f(k, candidates, res, v, rem, mp);
-        
-        
+
+        //include
+        v.push_back(arr[i]);
+        func(i+1,v, vv, arr, target, sum+arr[i]);
+        v.pop_back();
+
+        //exclude
+        int j = i+1;
+        while(j<arr.size() && arr[j] == arr[i])j++;
+        func(j,v, vv, arr, target, sum);
+
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>res;
-        vector<int>v;
-        map<int,bool>vis;
-        sort(candidates.begin(), candidates.end());
-        f(0, candidates, res, v, target, vis);
-        return res;
+       sort(candidates.begin(), candidates.end());
+       vector<vector<int>>vv;
+       vector<int>v;
+       int sum =0;
+       func(0, v, vv, candidates, target, sum);
+       return vv;    
     }
 };
+// 1 2 2 2 5 
