da0f2e3a251bc0b02dc57e21467eaf556143fe7c
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2024-12-30 22:53:21 +0530

    Time: 11 ms (28%), Space: 27.2 MB (72.32%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,23 +11,25 @@
  */
 class Solution {
 public:
-    TreeNode* f(vector<int>& preorder, vector<int>& inorder, int pre_l, int pre_r, int in_l, int in_r, unordered_map<int,int>&mp){
-        if(pre_l> pre_r || in_l > in_r) return NULL;
-        
-        TreeNode* node = new TreeNode(preorder[pre_l]);
-        
-        //find node's index in inorder
-        int node_id = mp[preorder[pre_l]];
-        int cnt_left = node_id- in_l;
-        //now make node->left and right subtree
-        node->left = f(preorder, inorder, pre_l+1, pre_l+cnt_left, in_l, node_id-1, mp);
-        node->right = f(preorder, inorder, pre_l+cnt_left+1, pre_r, node_id+1, in_r, mp);
-        
-     return node;   
+    TreeNode* f(vector<int>& preorder, vector<int>& inorder, int pl, int pr, int il, int ir){
+        if(pl>pr || il>ir) return NULL;
+        int nodeId = -1;
+        //find this node in inorder->
+        for(int i=il;i<=ir; i++){
+            if(inorder[i] == preorder[pl]){
+                nodeId=i;
+                break;
+            }
+        }
+        int cnt_left = nodeId - il;
+        TreeNode* node = new TreeNode(preorder[pl]);
+        node->left = f(preorder, inorder, pl+1, pl+cnt_left, il, nodeId-1 ); 
+        node->right =f(preorder, inorder, pl+cnt_left+1, pr, nodeId+1, ir);
+
+        return node;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int, int>mp;
-        for(int i=0;i<inorder.size(); i++)mp[inorder[i]] = i;
-        return f(preorder, inorder,0, preorder.size() -1, 0, inorder.size()-1, mp);
+
+        return f(preorder, inorder, 0, preorder.size()-1, 0, inorder.size()-1);
     }
 };
