9c34915d393e8013f1a640d261d33c19a1777fd2
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-01-12 23:46:25 +0530

    Time: 3 ms (33.8%), Space: 21.9 MB (21.17%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,41 +1,27 @@
 class Solution {
-public:
-    void addRange(set<pair<int,int>>&s, int left, int right){
-         auto it = s.lower_bound({left, right});
-         
-
-         if(it !=s.begin() && s.size()){
-            auto before = prev(it);
-            
-            if(left <= before->second ){
-                    left = min(left, before->first);
-                    right = max(right, before->second);
-                     s.erase(before);
-            }
-         }   
-
-         auto nxt = it;
-         while(nxt!=s.end() && nxt->first <=right){
-            right = max(right, nxt->second);
-            nxt++;
-            s.erase(prev(nxt));
-         }
-         s.insert({left, right});
+    static bool sortFunc(vector<int>&v1, vector<int>&v2){
+        if(v1[0]==v2[0]){
+            return v1[1]<v2[1];
+        }
+        return v1[0] < v2[0];
     }
-
+public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-
-        set<pair<int,int>>s;        
-        for(auto&x:intervals){
-            s.insert({x[0], x[1]});
-        }
-        addRange(s, newInterval[0], newInterval[1]);
-
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end(), sortFunc);
         vector<vector<int>>res;
-        
-        for(auto& x:s){
-            res.push_back({x.first, x.second});
+
+        int l = intervals[0][0], r= intervals[0][1];
+        for(int i=0;i<intervals.size() -1; i++){
+            if(intervals[i+1][0] <= r){
+                r= max(r, intervals[i+1][1]);
+            }else{
+                res.push_back({l, r});
+                l = intervals[i+1][0],r=intervals[i+1][1];
+            } 
         }
+
+        res.push_back({l, r});
 
         return res;
     }
