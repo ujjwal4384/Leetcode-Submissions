80eb35d4700323636404e48c382f1671ba1c626a
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-09 21:13:03 +0530

    Time: 6 ms (6.7%), Space: 10.2 MB (19.78%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValid(string&s){
+        int sz = s.size();
+        switch(sz){
+            case 0: 
+               return false;
+            case 1:
+                return s > "0" && s<="9";
+            case 2: 
+                 return s[0] > '0' && s>"0" && s <="26";
+            default:
+                  return false;            
+        }
+      return false;  
+    }
+    int f(int i, string&s, int dp[]){
+        if(i >= s.size()){
+            return 1;
+        }
+        if(dp[i] != -1) return dp[i];
+
+        string t = "";
+        int count = 0;
+        for(int k=i; k< s.size(); k++){
+            t += s[k];
+            bool flag = isValid(t);
+            if(flag){
+                count += f(k+1, s, dp);
+            }
+        }
+
+        return  dp[i] = count;
+    }
+    int numDecodings(string s) {
+        int n = s.size();
+        int* dp = new int[n+1];
+        fill_n(dp, n+1, -1);
+        return f(0, s, dp);
+    }
+};
