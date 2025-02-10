0a0808474c0499a4add47d40065d7ab52744ecc7
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-11 02:06:26 +0530

    Time: 29 ms (12.56%), Space: 11.4 MB (62.79%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isValid(map<char, int>&mp, map<char, int>&freq){
+        for(auto p : freq){
+            char x = p.first;
+            int count = p.second;
+            if(mp[x] < count) return false;
+        }
+       return true; 
+    }
+
+    string minWindow(string s, string t) {
+        map<char, int>freq, mp;
+        for(auto&x:t)freq[x]++;
+
+        int m = s.size(), n= t.size();
+        
+        int len = m+n+1;
+        int start = -1;
+        
+        for(int st=0,en=0; en<m; en++){
+            
+            mp[s[en]] ++ ;
+            
+            if(isValid(mp, freq)){
+                
+                while(st<=en){
+                    int need = freq[s[st]];
+                    int cur = mp[s[st]];
+                    if(cur - need > 0){
+                        mp[s[st]]--;
+                        st++;
+                    }else{
+                        break;
+                    }
+                }
+                
+                int curLen = en - st + 1;
+                if(curLen < len){
+                    len = curLen;
+                    start = st;
+                }
+
+                mp[s[st]] --;
+                st++;
+            }
+        }
+
+        return len==m+n+1 ? "" : s.substr(start, len);
+    }
+};
