26875c2f35b496da3ccf53e948d094b663b4af72
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-11 17:44:20 +0530

    Time: 0 ms (100%), Space: 13.2 MB (87.01%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+            int n = nums.size();
+            int ans = 0;      
+
+            for(int bit = 0; bit<32; bit ++){
+                int p = (1<<bit);
+                int totalBitSet = 0;
+                for(auto x:nums){
+                       if(x & p){
+                           totalBitSet ++; 
+                       } 
+                }
+
+                if(totalBitSet % 3){
+                    ans += p;
+                }
+            }
+
+           return ans; 
+    }
+};
