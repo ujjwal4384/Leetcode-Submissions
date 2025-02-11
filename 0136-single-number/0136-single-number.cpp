d1829792a884c72cc79b22aa94d5eadc69bec919
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-11 17:47:17 +0530

    Time: 8 ms (20.31%), Space: 20.6 MB (83.69%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,12 +2,20 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        int xorr = 0;
-        for(int i=0; i<n ;i++){
-            xorr = xorr ^ nums[i];
-
+        int ans =0;
+        for(int bit =0;bit<32;bit++){
+            int p = (1<<bit);
+            int count = 0;
+            for(auto x: nums){
+                if(x & p){
+                    count++;
+                }
+            }
+            if(count%2){
+                ans = ans | p;
+            }
         }
 
-        return xorr;
+        return ans;
     }
 };
