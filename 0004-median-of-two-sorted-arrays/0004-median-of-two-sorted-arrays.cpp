c5af692363ee32414b540dd665c39c9e9d70b773
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-11 14:56:04 +0530

    Time: 1 ms (43.19%), Space: 95 MB (80.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
-        int n1 = v1.size();
-        int n2 = v2.size();
-       
-     
-        int n = n1 + n2;
-        
-        int left = (n + 1) / 2;
-        
-        int low1 = max(0, left-n2), high1 = min(n1, left);
-        
-        int l1, l2, r1, r2;
-        
-        while (low1 <= high1) {
-            int mid1 = (low1 + high1) >> 1;
-            int mid2 = left - mid1;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int sz1 = nums1.size() ;
+        int sz2 = nums2.size() ;
 
-            l1 = mid1 - 1 >= 0 ? v1[mid1 - 1] : INT_MIN;
-            l2 = mid2 - 1 >= 0 ? v2[mid2 - 1] : INT_MIN;
-            r1 = mid1 < n1 ? v1[mid1] : INT_MAX;
-            r2 = mid2 < n2 ? v2[mid2] : INT_MAX;
+        int elementsLeftSide = (sz1 + sz2 + 1)/2 ;
+        
+        int st = max(0, elementsLeftSide - sz2);
+        int en = min(elementsLeftSide, sz1);
+        
+        while(st<=en){
+                int mid1 = (st+en)/2 ;
+                int mid2 =  elementsLeftSide - mid1 ;
+               // cout<<mid1<<", "<<mid2<<endl; 
+                int l1 = mid1-1>=0 ? nums1[mid1-1] :  INT_MIN  ;
+                int r1 = mid1 < sz1 ? nums1[mid1] : INT_MAX;
+                int l2 = mid2-1>=0 ? nums2[mid2-1] : INT_MIN;
+                int r2 = mid2 < sz2 ? nums2[mid2] : INT_MAX;
 
-            if (l1 <= r2 && l2 <= r1) {
-                break;
-            } else if (l1 > r2) {
-                high1 = mid1 - 1;
-            } else {
-                low1 = mid1 + 1;
-            }
+                if(l1<=r2 && l2<=r1){
+                    if( (sz1+sz2)%2==0){
+                        // cout<<"l1="<<l1<<",r1="<<r1<<",l2="<<l2<<",r2="<<r2<<endl;
+                        return (max(l1, l2) + min(r1, r2))/2.0 ; 
+                    }else{
+                        return max(l1, l2);
+                    }
+                    
+                }
+                else if(l1 > r2){
+                    en = mid1 - 1 ;
+                }
+                else{
+                    st = mid1 + 1;
+                }
+                
         }
 
-        return n % 2 ? max(l1, l2) : (max(l1, l2) + min(r1, r2)) / 2.0;
+         return 0.0;
     }
-    
 };
