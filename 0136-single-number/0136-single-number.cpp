a9b0e2cc708c50ff8d41674f302109c527bd98af
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-11 15:11:52 +0530

    Time: 0 ms (100%), Space: 20.7 MB (59.21%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xorr = 0;
+        for(int i=0; i<n ;i++){
+            xorr = xorr ^ nums[i];
+
+        }
+
+        return xorr;
+    }
+};
