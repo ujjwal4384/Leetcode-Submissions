8630eaa7597c53f611916c24904534f469f91d76
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-16 03:37:45 +0530

    Time: 0 ms (100%), Space: 12.7 MB (94.04%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,31 +1,28 @@
 class Solution {
-int m,n;    
-vector<vector<int>>dp;
 public:
-    int f(int i, int j, vector<vector<int>>& dungeon){
-        if(i==m-1 && j==n-1){
-            return dungeon[i][j] >=0 ? 0: dungeon[i][j];
-        }
-        if(i>=m || j>=n) return INT_MIN;
-        if(dp[i][j]!=INT_MIN) return dp[i][j];
-        int way1, way2= INT_MIN;
-        way1= f(i+1, j, dungeon);
-        way2= f(i,j+1, dungeon);
-        int optimal = max(way1, way2) + dungeon[i][j];
-        if(optimal>=0) return dp[i][j] = 0;
-        
-        return dp[i][j] = optimal;
-        
-    }
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-       
-        dp.clear();
-       
-        m=dungeon.size(), n=dungeon[0].size();
-         dp = vector<vector<int>>(m, vector<int>(n, INT_MIN));
-        return -1*f(0,0, dungeon)+1;
+        int m = dungeon.size();
+        int n= dungeon[0].size();
+
+        int dp[201][201];
+        memset(dp, 0, sizeof(dp));
+        //dp[i][j] = min health needed in i,j to reach m-1*n-1 successfully.
+        dp[m-1][n-1] = max(1, -1*dungeon[m-1][n-1] + 1);
+
+        for(int i=m-2;i>=0;i--){
+            dp[i][n-1] = max(1, dp[i+1][n-1] - dungeon[i][n-1]);
+        }
+
+        for(int j=n-2;j>=0;j--){
+            dp[m-1][j] = max(1, dp[m-1][j+1] - dungeon[m-1][j]);
+        }
+
+        for(int i=m-2; i>=0; i--){
+            for(int j=n-2; j>=0; j--){
+                dp[i][j] = max(1, min(dp[i+1][j],dp[i][j+1]) - dungeon[i][j] ) ;
+            }
+        }
+
+       return dp[0][0]; 
     }
 };
-
-
-//i,j tak aane me need?: i-1,j tak ki need=10. i-1, j. 5 
