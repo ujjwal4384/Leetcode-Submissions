f321d46542d92a0830686ce15319e8c089eded83
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-17 22:55:57 +0530

    Time: 2958 ms (5.19%), Space: 9 MB (45.25%) - LeetHub

---
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    int solve(string& num, int N, bool takeBoth, bool tight){
+        if(N==1){
+            return takeBoth==false ? 1 : ( tight ? (num[num.size()-N]-'0') + 1 :2);
+        }
+        
+        int ub = ( tight ? (num[num.size()-N]-'0') :1);
+        int count = 0;
+
+        for(int dig =0; dig<=ub; dig++){
+              if(dig==1 && takeBoth==false){
+                continue;
+              }
+              count +=   solve(num, N-1, dig==0?true:false, tight && dig==ub);
+        }
+
+        return count;
+    }
+
+    int findIntegers(int n) {
+        string s = "";
+       
+        for(int bit = 0; bit<32; bit++){
+            int p = (1<<bit);
+            if(n&p){
+                s = '1' + s;
+               
+            }else{
+                s = '0' + s;
+            }
+        }
+        int i = 0;
+        string temp="";
+        while(s[i]=='0')i++;
+        temp = s.substr(i);
+     
+
+        cout<<temp<<" \n";
+        int ans = solve(temp, temp.size(), true, 1);
+      //  cout<<ans<<endl;
+        return ans;
+    }
+};
