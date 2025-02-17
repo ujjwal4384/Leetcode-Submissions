4faf8bf207094e19f4ad056e2fd79542dd70cc1f
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-18 01:06:21 +0530

    Time: 2 ms (18.42%), Space: 7.8 MB (91.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
-    int uniquePathsCombinatorial(int m, int n) {
-        if(m==1 || n==1) return 1;
-        //math.comb(m+n-2, m-1)
-        //total steps= m-1 + n-1 = m+n-2.//just find ways of moving down,bcz corresponding to each such way there is only one way to go right on remaining steps.
-        //(m+n-2)!/(m-1)!(n-1)!  
-        
-        //(m+n-2)*....(m)/(n-1)!
-        if(m<n)swap(m,n);
-        long long ans=1;
-        for(int i=1; i<=n-1; i++){
-            long long num = (m+n-1-i);
-            long long den = (i);
-            ans=  1LL * ans * num/den;
+    int f(int i, int j, int dp[101][101]){
+        if(i==0 && j==0){
+            return 1;
         }
-        return (int)ans;
-        
-    }
-    int f(int i, int j, int dp[][101]){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0){
+        else if(i<0 || j<0){
             return 0;
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        return dp[i][j] = f(i-1, j, dp) + f(i, j-1, dp);
+        else if(dp[i][j] != -1) return dp[i][j];
+
+        int fromTop = 0,fromLeft=0;
+        fromTop = f(i-1, j, dp);
+        fromLeft = f(i,j-1, dp);
+
+        return dp[i][j] =  fromTop + fromLeft;
     }
     int uniquePaths(int m, int n) {
-        if(m==1 || n==1) return 1;
         int dp[101][101];
-         memset(dp,-1,sizeof(dp));
-         int ans = f(m-1, n-1, dp);
-        return ans;
-        
+        memset(dp, -1, sizeof(dp));
+        return f(m-1, n-1, dp);
     }
 };
