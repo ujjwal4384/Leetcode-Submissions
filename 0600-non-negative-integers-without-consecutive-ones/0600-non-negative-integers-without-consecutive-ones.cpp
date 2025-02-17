aea2dbd195e129288d88174d6ad8b60568cc419e
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-02-17 22:58:28 +0530

    Time: 0 ms (100%), Space: 9.2 MB (42.08%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
 
-    int solve(string& num, int N, bool takeBoth, bool tight){
+    int solve(string& num, int N, bool takeBoth, bool tight,int dp[33][2][2] ){
         if(N==1){
             return takeBoth==false ? 1 : ( tight ? (num[num.size()-N]-'0') + 1 :2);
         }
-        
+        else if(dp[N][takeBoth][tight] !=-1) return dp[N][takeBoth][tight];
+
         int ub = ( tight ? (num[num.size()-N]-'0') :1);
         int count = 0;
 
@@ -13,10 +14,10 @@ public:
               if(dig==1 && takeBoth==false){
                 continue;
               }
-              count +=   solve(num, N-1, dig==0?true:false, tight && dig==ub);
+              count +=   solve(num, N-1, dig==0?true:false, tight && dig==ub, dp);
         }
 
-        return count;
+        return dp[N][takeBoth][tight] = count;
     }
 
     int findIntegers(int n) {
@@ -35,14 +36,9 @@ public:
             }
         }
         
-        // int i = 0;
-        // string temp="";
-        // while(s[i]=='0')i++;
-        // temp = s.substr(i);
-     
-
+        
       
-        int ans = solve(s, s.size(), true, 1);
+        int ans = solve(s, s.size(), true, 1, dp);
     
         return ans;
     }
