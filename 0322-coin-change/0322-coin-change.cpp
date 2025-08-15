8f7c5dc07591fe0b9312e371bb3a3207e2d75f28
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-15 22:48:04 +0530

    Time: 23 ms (73.24%), Space: 17.9 MB (75.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,51 +1,17 @@
 class Solution {
 public:
-    int f(int i, vector<int>& coins, int amount, vector<vector<int>>&dp){
-        //BASE CASE
-        if(i==0){
-            //...
-            if(amount ==0){
-                return 0;
-            }
-            return amount>=coins[0] && amount%coins[0]==0 ? amount/coins[0] : 1e5;
-        }
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        int take=1e5, notTake=1e5;
-        notTake = 0 + f(i-1, coins, amount, dp);
-        if(amount-coins[i]>=0){
-            take = 1 + f(i, coins, amount-coins[i], dp);
-        }
-
-        return dp[i][amount] =min(take, notTake);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        
-        //solve like kanpsack. 
-        //0-1 kanpsack em we had ot maximize the value, here minimuize the number of coins. here we have infinite supply.
         int n= coins.size();
-        // vector<vector<int>>dp(n, vector<int>(amount+1, -1));
-        // int ans = f(n-1, coins, amount, dp);
-    
-        // vector<vector<int>>dp(n, vector<int>(amount+1, 0));    
-        vector<int>prev(amount+1,0);
-        for(int am=1;am<=amount;am++){
-            prev[am] = am>=coins[0] && am%coins[0]==0 ? am/coins[0] : 1e5;
-        }
-
-        for(int i=1;i<n;i++){
-            for(int am =0 ; am<=amount; am++){
-                  
-                int take=1e5, notTake=1e5;
-                notTake = 0 + prev[am];
-                if(am-coins[i]>=0){
-                    take = 1 + prev[am-coins[i]];
+        vector<int>dp(amount+1,amount+1);
+        dp[0] =0;
+       
+        for(int amt=0;amt<=amount;amt++){
+            for(int i=0;i<n;i++){
+                if(amt>=coins[i]){
+                    dp[amt] = min(dp[amt],1 + dp[amt-coins[i]]);
                 }
-
-                prev[am] =min(take, notTake);
             }
-            
         }
-        int ans = prev[amount];
-        return ans ==1e5?-1:ans;
+        return dp[amount] == amount+1 ? -1 : dp[amount];
     }
 };
