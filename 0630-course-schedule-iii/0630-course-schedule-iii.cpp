6efd20a1fcd3125163dbaff29388c8a06f1dc6e4
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-16 20:10:55 +0530

    Time: 80 ms (13.83%), Space: 73.1 MB (11.72%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,35 @@
+bool comp(vector<int>&v1, vector<int>&v2){
+    if(v1[1] == v2[1]){
+        return v1[0] < v2[0];
+    }
+    return v1[1] < v2[1];
+}
+class Solution {
+
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), comp);
+        multiset<int, greater<int>>s;
+        int time =0;
+        for(auto course:courses){
+            int duration = course[0];
+            int deadline = course[1];
+            if(duration > deadline){
+                continue;
+            }
+
+            if(time + duration <= deadline){
+                   s.insert(duration); 
+                   time += duration;
+            }else{
+                //can swapping be helpful?
+                if (s.size() && (* s.begin()) > duration){
+                    time = time - (* s.begin()) + duration;
+                    s.erase(s.begin());
+                    s.insert(duration);
+                }
+            }
+        }
+        return s.size();
+    }
+};
