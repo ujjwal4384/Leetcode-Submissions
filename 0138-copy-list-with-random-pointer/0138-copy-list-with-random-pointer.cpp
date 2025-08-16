b2b92958d15efe461bf57cbabe84d51e15fef87e
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-16 21:29:11 +0530

    Time: 5 ms (65.44%), Space: 15 MB (84.86%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,24 +17,42 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-    
+       if(!head) return NULL;
+
        Node* temp = head;
 
-       map<Node*,Node*>mp;
-
+      
+        // step-1: interweave. add new node in middle of two old nodes.
        while(temp){
-        mp[temp] = new Node(temp->val);
-        temp=temp->next;  
+        Node* newNode = new Node(temp->val);
+        newNode->next = temp->next;
+        temp->next = newNode;
+        temp = temp->next->next;
        } 
 
-       //assign random and next pointers
+       // step-2: assign random pointer for new nodes
        temp = head; 
        while(temp){
-          mp[temp]->next = mp[temp->next];
-          mp[temp]->random = mp[temp->random];
-          temp = temp->next;
-       }
+             temp->next->random = temp->random ? temp->random->next: NULL;
+             temp = temp->next->next;   
+       } 
+       
+      //step-3: separate new node list from old.  
+      Node* newHead = head->next;
+      temp = head;
+      while(temp){
+            Node* curOldNode = temp;
+            Node* nextOldNode = temp->next->next;
 
-       return mp[head];
+            Node* curNewNode = temp->next;
+            Node* nextNewNode = nextOldNode ? nextOldNode->next : NULL;
+
+            curOldNode->next = nextOldNode;
+            curNewNode->next = nextNewNode;
+
+            temp = nextOldNode;
+      }
+       
+      return newHead;
     }
 };
