e4f12941967a2adcac523906254e31f5e6e89016
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-16 17:04:55 +0530

    Time: 8 ms (37.39%), Space: 20.1 MB (31.39%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
+    bool isCycleExist(int node, vector<vector<int>>&adj, vector<bool>&vis,vector<bool>&recVis){
+        vis[node] = true;
+        recVis[node] = true;
+        for(auto x: adj[node]){
+            if(recVis[x]) return true;
+            if(!vis[x] && isCycleExist(x, adj, vis, recVis)){
+                return true;        
+            }
+        }
+        
+        recVis[node] = false;
+        return false;
+    }
     bool canFinish(int n, vector<vector<int>>& v) {
          vector<vector<int>>adj(n); 
          for(auto x:v){
             adj[x[1]].push_back(x[0]);
          }
-
-         map<int,int>indegree;
-
-         for(auto x: adj){
-            for(auto y:x){
-                indegree[y]++;
-            }
+         vector<bool>vis(n, false), recVis(n, false);   
+         for(int i=0 ;i<n; i++){
+            if(!vis[i] && isCycleExist(i, adj, vis, recVis)){
+                return false;
+            } 
          }
-
-         queue<int>q;   
-         for(int i=0; i<n ;i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
-         }
-
-         vector<int>res;
-         while(!q.empty()){
-              int node = q.front();
-              q.pop();
-              res.push_back(node);
-              for(auto x: adj[node]){
-                 indegree[x]--;
-                 if(indegree[x] ==0){
-                    q.push(x);
-                 }
-              }
-         }
-
-         return res.size() == n;    
+         return true;
     }
 };
