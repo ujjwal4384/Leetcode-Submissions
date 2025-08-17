c4a7ef2c909704bb2df72c25815c055653479d0b
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-17 20:51:06 +0530

    Time: 0 ms (100%), Space: 11.9 MB (70.92%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,41 +11,19 @@
  */
 class Solution {
 public:
-    void f(TreeNode* root, vector<string>&v, string s){
+    int f(TreeNode* root, int cur){
         if(!root){
-            return;
+            return 0;
         }
-        int val = root->val;
-        string val_str = to_string(val);
-        string s_new = s + val_str;
+        cur = cur*10 + root->val ;
         if(!root->left && !root->right){
-            v.push_back(s_new);    
-            return ;
+            return cur;
         }
-        
-        f(root->left, v, s_new);
-        f(root->right, v, s_new);
+
+        return f(root->left, cur) + f(root->right, cur);
     } 
 
-    int getNumberFromString(string& s){
-        int n =0;
-        int multiplier = 1;
-        for(int i=s.size()-1;i>=0;i--){
-            n = n + (s[i]-'0')*multiplier;
-            if(i>0)multiplier *= 10;
-        }
-        return n;
-    }
-
     int sumNumbers(TreeNode* root) {
-        vector<string>v;
-        string s="";
-        f(root, v, s);
-        int sum = 0;
-        for(auto x:v){
-                int num = getNumberFromString(x);
-                sum += num;
-        }
-        return sum;
+        return f(root, 0);
     }
 };
