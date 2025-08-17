6b77827a13792113b00653ad5f9c03ef01c02b6d
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-18 00:44:03 +0530

    Time: 0 ms (100%), Space: 11 MB (11.73%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,58 +11,26 @@
  */
 class Solution {
 public:
-    void f(TreeNode* root, vector<int>&v){
-        if(!root) return ;
-        f(root->left, v);
-        v.push_back(root->val);
-        f(root->right, v);
-        return ;
-    }
-
-    void f2(TreeNode*root, vector<int>&v){
-        stack<TreeNode*>st;
-        TreeNode* cur=root;
-         while(cur || !st.empty()){
-              if(cur){
-                TreeNode* temp=cur;
-                while(temp && temp->left){
-                    st.push(temp);
-                    temp= temp->left;
-                }
-                v.push_back(temp->val);
-                cur= temp->right;
-              }else{
-                TreeNode* x = st.top();
-                st.pop();
-                v.push_back(x->val);
-                cur = x->right;
-              }      
-        }
-    }
     vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;
+        TreeNode* cur = root;
         vector<int>v;
-        //f(root, v);
-        if(!root) return v;
-       // f2(root, v);
-
-       TreeNode*cur=root;
-       while(cur){
-        if(cur->left){
-                TreeNode* temp = cur->left;
-                while(temp->right != NULL && temp->right != cur)temp=temp->right;
-                if(temp->right == NULL){
-                    temp->right = cur;
-                    cur= cur->left;
+        while(cur || !st.empty()){
+                if(cur){
+                    TreeNode* tmp = cur;
+                    while(tmp->left){
+                        st.push(tmp);
+                        tmp = tmp->left;
+                    }
+                    v.push_back(tmp->val); 
+                    cur = tmp->right;
                 }else{
-                    v.push_back(cur->val);
-                    temp->right = NULL;
-                    cur=cur->right;
+                    TreeNode* tmp = st.top();
+                    st.pop();
+                    v.push_back(tmp->val);
+                    cur = tmp->right;
                 }
-        }else{
-            v.push_back(cur->val);
-            cur= cur->right;
         }
-       }
-        return v;
+      return v;
     }
 };
