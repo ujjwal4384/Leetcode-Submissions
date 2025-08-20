cc16e00ea9e3da9f89f239771bdafb4e2b4abd8a
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-20 23:36:09 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        int x_sign= x>=0?1:-1;
-        int n_sign= n>=0?1:-1;
-        int original_n = n;
-        double ans = 1;
-        if(n==0)return ans;
-        else if(x==0) return 0;
-        x=abs(x);
-        while(n){
-            if(n%2){
-                ans*=x;
-            }
-                x*=x;
-            
-            n/=2;
-        }
-       if(n_sign==-1)ans=1/ans;
-       if(x_sign==-1)ans= original_n%2?-1*ans:ans;
-       return ans; 
+    double f(double x, long long n){
+        if(x==0) return 0;
+        if(x==1) return 1;
+        if(n==0) return 1;
+
+        double multiplier = (n%2==1) ? x : 1;
+        
+        x *= x;
+        n /= 2;
+        return  multiplier * f(x, n);
+    }
+
+    double myPow(double x, int _n) {
+         long long int n = _n;
+         int sign_n = n<0 ? -1 : 1;
+         int sign = (x<0 && n%2) ? -1 : 1;
+         
+         double res = f(abs(x), abs(n));        
+      
+
+        if(sign_n == -1){
+            res = 1/res;
+        } 
+        return sign * res;
     }
 };
-
-
-// 2^30=> (4)^15=> 4*4^14=> 4* (16)^7;
