81ce65bda4316d3898742f3a2fc0d4888dde9ea4
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-26 03:20:21 +0530

    Time: 0 ms (100%), Space: 18 MB (64.95%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {    
-        vector<vector<pair<int,int>>>adj(n);
-        
-        for(auto edge:flights){
-            int u=edge[0],v=edge[1],w=edge[2];
-            adj[u].push_back({v,w});
+        vector<vector<pair<int,int>>> adj(n);
+        for (auto &edge : flights) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            adj[u].push_back({v, w});
         }
-        queue<int>q;
-        vector<int>dist(n, INT_MAX);
-    
-        dist[src] = 0; 
-        q.push(src);
-        int steps = 0;
 
-        while(!q.empty()){
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+
+        queue<int> q;
+        q.push(src);
+
+        int steps = 0;
+        while (!q.empty() && steps <= k) {
             int sz = q.size();
-            while(sz--){
-                int node =q.front();
+            vector<int> temp = dist;  // keep old distances for this level
+
+            while (sz--) {
+                int node = q.front(); 
                 q.pop();
-                if(steps>k && node != dst){
-                    continue;
-                }
-                if(steps> k+1 && node == dst){
-                    continue;
-                }
-                
-                for(auto p:adj[node]){
-                    int adjNode = p.first;
-                    int w = p.second;
-                    if(0LL + dist[node] + w  < dist[adjNode]){
-                        dist[adjNode] = dist[node] + w;
+
+                for (auto [adjNode, w] : adj[node]) {
+                    if (dist[node] != INT_MAX && dist[node] + w < temp[adjNode]) {
+                        temp[adjNode] = dist[node] + w;
                         q.push(adjNode);
                     }
-                }    
+                }
             }
+
+            dist = temp;  // update after finishing this layer
             steps++;
-            
         }
-       return dist[dst] == INT_MAX ? -1 : dist[dst];
+
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
     }
 };
