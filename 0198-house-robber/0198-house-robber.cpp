d1f42d8eb8076b89f14834eb2f126ad47b753a20
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-28 00:50:29 +0530

    Time: 0 ms (100%), Space: 10.3 MB (73.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -17,14 +17,30 @@ public:
         }
         return 0;
     }
+
+    //   vector<int>cur(2,0), nxt(2, 0);
+    //      for(int i= n-1; i>=0; i--){
+    //         for(int take =0 ; take<=1; take++){
+    //              if(take){
+    //                  cur[take] = nums[i] +  nxt[0] ;
+    //              }
+    //              else{ 
+    //                  cur[take] = max(nxt[1], nxt[0]);
+    //              }
+    //         }
+    //         nxt = cur;
+    //      }
+
+    //      return max(nxt[1], nxt[0]);
+
     int rob(vector<int>& nums) {
         int dp[103][2];
         memset(dp, 0, sizeof(dp));
-
+        vector<int>cur(2,0),nxt(2,0);
         for(int i=nums.size()-1;i>=0;i--){
             for(int canTake = 0;canTake<=1;canTake++){
                 if(canTake){
-                    int a = nums[i] + dp[i+2][canTake];
+                    int a = nums[i] + dp[i+1][!canTake];
                     int b = dp[i+1][canTake];
                     dp[i][canTake] = max(a, b);
                 }else{
