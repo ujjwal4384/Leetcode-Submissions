3de3793e978c7d785d9fe33df24b42de04703f96
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-27 12:39:52 +0530

    Time: 83 ms (19.07%), Space: 107.7 MB (11%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,30 @@
+auto comp=[](vector<int>&v1, vector<int>&v2){
+    if(v1[0] == v2[0]) return v1[1] < v2[1];
+    return v1[0] < v2[0];
+};
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& intervals) {
+         
+        sort(intervals.begin(), intervals.end(), comp);
+        int n=intervals.size();
+        int l=intervals[0][0], r = intervals[0][1];
+        int count = 1;
+        for(int i=1;i<n;i++){
+               vector<int>interval = intervals[i];
+               int st = interval[0], en= interval[1];
+               if(st <=r){
+                  r = min(r, en);
+                  continue;
+               }
+               else{
+                  count ++; 
+                  l=st, r = en;
+               }
+        }
+        
+
+      return count;  
+    }
+};
