916e8bb096019f37660760bf74f715d8da7f2e48
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-27 12:21:21 +0530

    Time: 7 ms (41.64%), Space: 25.4 MB (5.74%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,29 +1,27 @@
+auto comp=[](vector<int>&v1, vector<int>&v2){
+    if(v1[0] == v2[0]) return v1[1] < v2[1];
+    return v1[0] < v2[0];
+};
 class Solution {
-    static bool sortFunc(vector<int>&v1, vector<int>&v2){
-        if(v1[0]==v2[0]){
-            return v1[1]<v2[1];
-        }
-        return v1[0] < v2[0];
-    }
-
-    public:
-   
+public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), sortFunc);
         vector<vector<int>>res;
-
-        int l = intervals[0][0], r= intervals[0][1];
-        for(int i=0;i<intervals.size() -1; i++){
-            if(intervals[i+1][0] <= r){
-                r= max(r, intervals[i+1][1]);
-            }else{
-                res.push_back({l, r});
-                l = intervals[i+1][0],r=intervals[i+1][1];
-            } 
+        sort(intervals.begin(), intervals.end(), comp);
+        int n=intervals.size();
+        int l=intervals[0][0], r = intervals[0][1];
+        for(int i=1;i<n;i++){
+               vector<int>interval = intervals[i];
+               int st = interval[0], en= interval[1];
+               if(st <=r){
+                  r = max(r,en);
+               }
+               else{
+                  res.push_back({l, r});
+                  l=st, r = en;
+               }
         }
+        res.push_back({l,r});
 
-        res.push_back({l, r});
-
-        return res;
-    }
+      return res;  
+    } 
 };
