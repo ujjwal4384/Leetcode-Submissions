b61ef5d3ca14df6ca457158acd70d6488a77af0d
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2025-08-27 16:33:48 +0530

    Time: 3 ms (71.7%), Space: 15.5 MB (71.21%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    void f(int left, int right, string s, vector<string>&res){
-        if(left==0 && right==0){
+    void f(int open, int close, string s, vector<string>&res){
+        if(open == 0 && close == 0){
             res.push_back(s);
             return;
         }
-        
-        else if(left>right){
+
+        if(open > close){
             return ;
         }
-
-        if(right){
-            f(left, right-1, s+")", res);
+        if(open){
+            f(open-1, close, s+"(", res);
         }
-        if(left){
-            f(left-1, right, s+"(", res);
+        if(close){
+            f(open, close-1, s+")", res);
         }
-        return ;
     }
+    
     vector<string> generateParenthesis(int n) {
         vector<string>res;
-        
-        string s=""; 
-        f(n,n, s, res);   
+        string s="";
+        f(n, n, s, res);
         return res;
     }
 };
