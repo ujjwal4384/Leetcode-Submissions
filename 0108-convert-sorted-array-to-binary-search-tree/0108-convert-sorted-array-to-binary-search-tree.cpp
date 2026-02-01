5810615b506c17370cf9388231040a196a9c0e48
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-02-02 00:47:45 +0530

    Time: 7 ms (13.32%), Space: 22.9 MB (55.34%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -11,32 +11,17 @@
  */
 class Solution {
 public:
-    TreeNode* f1(int l, int r, vector<int>& nums){
-       if(l>r) return NULL;
-       int m = (l+r)/2 ;
-       int key = nums[m];
-       TreeNode* node = new TreeNode(key);
-       
-       node->left = f1(l, m-1, nums);
-       node->right = f1(m+1, r, nums);
+    TreeNode* buildHeightBalancedBST(int l, int r, vector<int>&nums){
+        if(l>r) return NULL;
+        int m = l + ((r-l)>>1) ;
+        TreeNode* root = new TreeNode(nums[m]);
+        root->left = buildHeightBalancedBST(l, m-1, nums);
+        root->right = buildHeightBalancedBST(m+1, r, nums);
 
-       return node;
-    }
-    TreeNode* f(int& i, vector<int>& nums, int count){
-        if(count==0) return NULL;
-        TreeNode* left = f(i, nums, count/2);
-
-        TreeNode* node = new TreeNode(nums[i]);
-        node->left = left;
-        i++;
-        node->right = f(i, nums, count - count/2 -1);
-
-        return node;
+        return root;
     }
 
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-       //return f1(0, nums.size()-1, nums);
-       int i =0;
-       return f(i, nums, nums.size());
+        return buildHeightBalancedBST(0, nums.size()-1, nums);
     }
 };
