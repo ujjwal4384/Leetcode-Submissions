124713fad70806fb0a73b87fd2339245ebe63c62
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-02-03 01:06:11 +0530

    Time: 3 ms (53.67%), Space: 25.7 MB (43.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& v) {
-       
-        int n=v.size();
-        
-       //there can be at max 2 such elements.
-        
-        //following algo finds most 2 frequent numbers in array.
-        int cand1,cnt1=0;
-        int cand2,cnt2=0;
-        for(int i=0;i<n;i++){
-            //cand1 ,cand2 do not compete with each other consider them fighting elections for different seats.
-            if(cnt1==0&&v[i]!=cand2){
-                cand1=v[i];
-                cnt1=1;
-               
-            }
-            else if(cnt2==0&&v[i]!=cand1){
-                cand2=v[i];
-                cnt2=1;
-            }
-            else if(v[i]==cand1){
-               cnt1++;
-            }
-            else if(v[i]==cand2){
-                cnt2++;
-            }
-            else{
-                cnt1--,cnt2--;
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int k = 3;
+        map<int,int>candidates;
+        for(auto x:nums){
+            if(candidates.count(x) > 0 ) candidates[x]++;
+            else {
+                if(candidates.size() < k-1)candidates[x] = 1;
+                else{
+                    //remove one element from each of the candidates.
+                    auto it = candidates.begin();
+                    while(it != candidates.end()){
+                        it->second --;
+                        if(it->second ==0){
+                            it = candidates.erase(it);//outpose empty, remove it
+                        }else{
+                            it++;
+                        }
+                    }
+                }
             }
         }
-        
-        //check freq of these 2 elements which are potential candidates
-        int freq1=0,freq2=0;
-        for(auto&x:v){
-            if(x==cand1)freq1++;
-            else if(x==cand2)freq2++;
+
+    // Phase 2: The Verification
+    for (auto& pair : candidates) pair.second = 0; // Reset counts for verification
+    for (int num : nums) {
+        if (candidates.count(num)) {
+            candidates[num]++;
         }
-        
-        vector<int>ans;
-        if(freq1>n/3)ans.push_back(cand1);
-        if(freq2>n/3)ans.push_back(cand2);
-        return ans;
     }
+        
+    vector<int>res;
+    for(auto p:candidates){
+            if(p.second > n/k)res.push_back(p.first);
+     }
+    return res;
+ }
 };
