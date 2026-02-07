04c66eac512d4dab1e2f4b12008782cbaf925e25
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-02-08 01:17:10 +0530

    Time: 186 ms (5.62%), Space: 182.5 MB (5.04%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,49 +1,35 @@
 class MedianFinder {
-    priority_queue<int, vector<int>>pq1;
-    priority_queue<int, vector<int>, greater<int>>pq2;
+    multiset<int,greater<int>>left;
+    multiset<int>right;
+    
 public:
     MedianFinder() {
-    
+        
     }
     
     void addNum(int num) {
-
-        pq1.push(num);
-        int sz1 = pq1.size();
-        int sz2 = pq2.size();
-
-        if(sz1- sz2 > 1){
-            int maxiFromPq1 = pq1.top();
-            pq1.pop();
-            pq2.push(maxiFromPq1);
+        left.insert(num) ;
+          
+        if(right.size() && (*left.begin() > *right.begin())){
+            int curL = *left.begin();
+            int curR = *right.begin();
+            left.erase(left.begin());
+            right.erase(right.begin());
+            left.insert(curR);
+            right.insert(curL);
         }
-        // sz2-sz1 <=1 but pq1 top has bigger el now than pq2 top.
-        else if(sz2 >0 && pq1.top() > pq2.top()){
-                pq2.push(pq1.top());    
-                pq1.pop();
-                pq1.push(pq2.top());
-                pq2.pop();    
-        }
+
+        if(left.size() -right.size() > 1){
+            right.insert(*left.begin());
+            left.erase(left.begin());
+        } 
     }
     
     double findMedian() {
-
-        
-        
-        int sz1 = pq1.size();
-        int sz2 = pq2.size();
-        int n = sz1+sz2;
-        
-        if(n==0) return 0;
-           
-        
-        if(n%2){
-            //odd
-            return pq1.top();
-        }else{
-            //even
-            return (pq1.top() + pq2.top())/2.0 ;
-        }
+        int n = left.size() + right.size();
+        if(n==0) return -1;
+        if(n%2) return *left.begin();
+        return (0LL + *left.begin() + *right.begin())/2.0;
     }
 };
 
