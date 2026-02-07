f9c6a3395a5328693ecebab458fd6af2064ff0c1
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-02-08 01:58:50 +0530

    Time: 413 ms (5.03%), Space: 216.2 MB (5.19%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,23 +3,12 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int>ans;
         multiset<int>main;
-        multiset<int, greater<int>>reserve;
         for(int i=0;i<nums.size();i++){
             main.insert(nums[i]);
-            if(main.size() > 1){
-                reserve.insert(*main.begin());
-                main.erase(main.begin());  
-            }
 
             if(i>=k){
                 int toRem = nums[i-k];
-                if(main.find(toRem) != main.end()){
                     main.erase(main.find(toRem));
-                    main.insert(*reserve.begin());
-                    reserve.erase(reserve.begin());
-                }else{
-                    reserve.erase(reserve.find(toRem));
-                }
             }
             if(i>=k-1){
                 ans.push_back( *prev( main.end() ) );
