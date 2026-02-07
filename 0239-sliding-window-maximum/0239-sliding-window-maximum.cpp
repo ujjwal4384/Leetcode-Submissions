5a29f3d4d9795394f533a652a9e4d60d3d41a0ea
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-02-08 01:56:57 +0530

    Time: 667 ms (5.03%), Space: 314.2 MB (5.19%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-      int n=nums.size();
-      deque<int>dq;
-        //answer always in front. maintains dq in decreasing order by value
-      vector<int>ans;
-        for(int i=0;i<n;i++){
-          //removal of smaller elements which occured before.
-            while(!dq.empty() && nums[dq.back()] <= nums[i])dq.pop_back();
-           //add this el
-            dq.push_back(i);
-          
-            //remove the outside window id,  if valid;  
-            int remId= i-k;
-           while(!dq.empty() && dq.front() <=remId)dq.pop_front(); 
-            
-            
-         //add to answer   
-            if(i>=k-1)ans.push_back(nums[dq.front()]);
-      }
-     return ans;   
+        vector<int>ans;
+        multiset<int>main;
+        multiset<int, greater<int>>reserve;
+        for(int i=0;i<nums.size();i++){
+            main.insert(nums[i]);
+            if(main.size() > 1){
+                reserve.insert(*main.begin());
+                main.erase(main.begin());  
+            }
+
+            if(i>=k){
+                int toRem = nums[i-k];
+                if(main.find(toRem) != main.end()){
+                    main.erase(main.find(toRem));
+                    main.insert(*reserve.begin());
+                    reserve.erase(reserve.begin());
+                }else{
+                    reserve.erase(reserve.find(toRem));
+                }
+            }
+            if(i>=k-1){
+                ans.push_back( *prev( main.end() ) );
+            }
+        }
+        return ans;
     }
 };
