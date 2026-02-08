b44ebfb35abf0b9f579de22d6aadcc22fafa4acd
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-02-09 00:13:08 +0530

    Time: 29 ms (35.62%), Space: 139.2 MB (66.93%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n= nums.size();
         vector<int>ans;
-        multiset<int>main;
-        for(int i=0;i<nums.size();i++){
-            main.insert(nums[i]);
-
-            if(i>=k){
-                int toRem = nums[i-k];
-                    main.erase(main.find(toRem));
+        deque<int>dq;
+        //make sure front element is always maximum.
+        //when new guy comes and it is greater than back elements, keep poping the back elements because they can never be maximum going ahead.
+        for(int i=0;i<n;i++){
+            if(!dq.empty() && dq.front() == i-k){
+                dq.pop_front();
             }
+            while(!dq.empty() && nums[i] >= nums[ dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
             if(i>=k-1){
-                ans.push_back( *prev( main.end() ) );
+                ans.push_back(nums[dq.front()]);
             }
         }
-        return ans;
+
+      return ans;
     }
 };
