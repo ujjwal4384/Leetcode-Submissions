12416ba8dba1fdbc7cb8c138119f62f430fc9b5c
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-02-12 21:33:42 +0530

    Time: 0 ms (100%), Space: 9.9 MB (69.38%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkInclusion(string p, string s) {
+         int m = s.size();
+        int n = p.size();
+
+        vector<int>ans;
+        if(n>m) return false;
+
+        int charsNeedToMatch = n;
+        vector<int>requiredCount(125, 0);
+        for(auto& c:p)requiredCount[c]++;
+
+        int l =0,  r =0;
+        while(r<m){
+            char ch = s[r];
+            if(requiredCount[ch] > 0){
+                charsNeedToMatch --;
+            }
+            requiredCount[ch] --;
+            if(r-l+1 ==n){
+                if(charsNeedToMatch == 0){
+                    ans.push_back(l);
+                }
+                //remove previous old guy
+                char toRem = s[l];
+                requiredCount[toRem]++;
+                if(requiredCount[toRem] > 0)charsNeedToMatch++;
+                l++;
+            }
+            r++;
+        }
+        return ans.size()>0;
+    }
+};
