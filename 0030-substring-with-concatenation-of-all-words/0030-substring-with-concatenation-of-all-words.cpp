e9f36629b9e2bc99565b230909b775acc0c26623
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-02-13 00:34:19 +0530

    Time: 89 ms (14.09%), Space: 44.7 MB (11.3%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int m = s.size();
+        int numWords = words.size();
+        int wordLen = words[0].size();
+        vector<int>ans;
+        map<string,int>freqMap;
+        for(auto word:words)freqMap[word]++;
+
+        for(int i=0;i<wordLen;i++){
+                map<string,int>requiredCount = freqMap;
+                int wordsToMatch = numWords;
+                int l =i, r = i;
+                while(r+wordLen-1 < m){
+                    string word = s.substr(r, wordLen);
+                    if(requiredCount[word] >0){
+                       wordsToMatch--; 
+                    }
+                    requiredCount[word]--;
+                    int windowSize = r-l+ wordLen;
+
+                    //shrink
+                    if(windowSize == numWords*wordLen){
+                        if(wordsToMatch == 0)ans.push_back(l);
+                        string wordToRem = s.substr(l,wordLen);
+                        requiredCount[wordToRem]++;
+                        if(requiredCount[wordToRem] > 0) wordsToMatch++;
+                        l += wordLen;
+                    }
+                    r += wordLen;    
+                }
+        }
+      return ans;  
+    }
+};
