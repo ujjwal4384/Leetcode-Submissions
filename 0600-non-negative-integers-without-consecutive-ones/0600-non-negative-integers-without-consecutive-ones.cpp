35e4c1f1725eab596419cd376c76c020356978ef
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-03-11 09:56:33 +0530

    Time: 0 ms (100%), Space: 8.2 MB (81.01%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
-//digit dp
-    int solve(string& num, int N, bool takeBoth, bool tight,int dp[33][2][2] ){
-        if(N==1){
-            return takeBoth==false ? 1 : ( tight ? (num[num.size()-N]-'0') + 1 :2);
-        }
-        else if(dp[N][takeBoth][tight] !=-1) return dp[N][takeBoth][tight];
-
-        int ub = ( tight ? (num[num.size()-N]-'0') :1);
-        int count = 0;
-
-        for(int dig =0; dig<=ub; dig++){
-              if(dig==1 && takeBoth==false){
-                continue;
-              }
-              count +=   solve(num, N-1, dig==0?true:false, tight && dig==ub, dp);
-        }
-
-        return dp[N][takeBoth][tight] = count;
-    }
-
     int findIntegers(int n) {
-        string s = "";
-        int dp[33][2][2];
-        
-        memset(dp,-1, sizeof(dp));
+        int fib[33] ={0};
+        //fib [i] = no. of ways for string of length i.
+        fib[0] = 1;
+        fib[1] = 2;
+        for(int i=2;i<33;i++)fib[i] = fib[i-1] + fib[i-2];
 
-        for(int bit = 0; bit<32; bit++){
-            int p = (1<<bit);
-            if(n&p){
-                s = '1' + s;
-               
+        int ans =0;
+        int prev_bit = 0;
+        for(int i=31;i>=0;i--){
+            int bit = n & (1<<i);
+            if(bit == 0){
+                prev_bit = 0;
             }else{
-                s = '0' + s;
+                //if we put 0 in this pos, then all ways of length i
+                ans += fib[i];
+
+                //now we put 1. 
+                if(prev_bit ==1) return ans; //illegal formation 11 subsequent all numbres will be also illegal
+                prev_bit =1;
             }
         }
-        
-        
-      
-        int ans = solve(s, s.size(), true, 1, dp);
-    
-        return ans;
+        //if N doesnot itself ahs 11 then us case mei, N is also valid answer so add 1
+        return ans +1;
+
     }
 };
