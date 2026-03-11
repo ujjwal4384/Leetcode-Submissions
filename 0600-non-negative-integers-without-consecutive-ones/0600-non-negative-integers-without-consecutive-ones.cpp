7ff9c0747e4479d29b033a6b0751ad50c7cf0fe9
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-03-11 23:27:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -1,29 +1,35 @@
 class Solution {
+       string s="";
+    
+    int dp[32][2][2][2] ; //dp[idx][tight][leading_zero][property]   
 public:
-    int findIntegers(int n) {
-        int fib[33] ={0};
-        //fib [i] = no. of ways for string of length i.
-        fib[0] = 1;
-        fib[1] = 2;
-        for(int i=2;i<33;i++)fib[i] = fib[i-1] + fib[i-2];
-
-        int ans =0;
-        int prev_bit = 0;
-        for(int i=31;i>=0;i--){
-            int bit = n & (1<<i);
-            if(bit == 0){
-                prev_bit = 0;
-            }else{
-                //if we put 0 in this pos, then all ways of length i
-                ans += fib[i];
-
-                //now we put 1. 
-                if(prev_bit ==1) return ans; //illegal formation 11 subsequent all numbres will be also illegal
-                prev_bit =1;
-            }
+int solve(int idx, bool tight, bool leading_zero, int property){
+        if(idx == s.size()){
+            return 1;
         }
-        //if N doesnot itself ahs 11 then us case mei, N is also valid answer so add 1
-        return ans +1;
+        else if(dp[idx][tight][leading_zero][property] !=-1) return dp[idx][tight][leading_zero][property];
+        
+        int limit = (tight==1)? s[idx] - '0': 1;
+        int ways = 0;
 
+        for(int d=0;d<=1;d++){
+            if(tight && (d >limit)) {
+                continue;
+            }
+            if(d==1 && property==1 && leading_zero ==0) continue;
+            ways +=  solve(idx+1, tight & (d == limit), leading_zero && (d==0), d);
+        }
+        return dp[idx][tight][leading_zero][property] = ways;
+    }
+
+    int findIntegers(int n) {
+        memset(dp, -1, sizeof(dp));
+        int temp = n;
+        while(temp > 0) {
+            s += to_string(temp % 2);
+            temp /= 2;
+        }
+        reverse(s.begin(), s.end());
+        return solve(0,1,1,0);
     }
 };
