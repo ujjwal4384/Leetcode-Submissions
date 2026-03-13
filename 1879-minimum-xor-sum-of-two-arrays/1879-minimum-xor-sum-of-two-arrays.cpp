6f3d9c9903bae6b95e929b5e4b67f64fd478bd2a
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-03-14 02:04:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
@@ -1,24 +1,25 @@
 class Solution {
-    int dp[14][20000];//dp[i][mask]. i= cur position in nums1. mask= what elemtns have been already picked and what are remainig to be piocked in nums2. stores minium xor we can obtain starting from index 0 of nums1 where mask state of elements in nums2 are remainign to be picked.
+    int dp[20000];//dp[i][mask]. i= cur position in nums1. mask= what elemtns have been already picked and what are remainig to be piocked in nums2. stores minium xor we can obtain starting from index 0 of nums1 where mask state of elements in nums2 are remainign to be picked.
 public:
-    int solve(int i, int mask, vector<int>& nums1, vector<int>& nums2){
+    int solve(int mask, vector<int>& nums1, vector<int>& nums2){
+        int i = __builtin_popcount(mask) ;//count of 1s in mask => how many nums2 have been paired => which index of nums1 w want tomc urrently look for is i. //O(1)
         if(i==nums1.size()){
             return 0;
         }
-        else if(dp[i][mask] !=-1) return dp[i][mask];
+        else if(dp[mask] !=-1) return dp[mask];
         int ans = INT_MAX;    
         for(int j=0;j<nums2.size(); j++){
               bool canPickThisElement = (mask & (1<<j)) ==0;
               if(canPickThisElement){
-                   int cur = (nums1[i] ^ nums2[j])  + solve(i+1, mask | (1<<j), nums1, nums2);
+                   int cur = (nums1[i] ^ nums2[j])  + solve(mask | (1<<j), nums1, nums2);
                    ans = min(ans, cur); 
               }   
         }
-        return dp[i][mask] = ans;
+        return dp[mask] = ans;
     } 
 
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
         memset(dp, -1, sizeof(dp));
-        return solve(0, 0, nums1, nums2) ;
+        return solve( 0, nums1, nums2) ;
     }
 };
