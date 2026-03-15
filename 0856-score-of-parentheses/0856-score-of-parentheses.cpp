f15e4c67ed9c15ac19309f10e219f3b3d6279829
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-03-15 23:52:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.cpp
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-         stack<int>st;
-         //We use the stack to hold the running score of each unfinished layer.
-         st.push(0); //base layer to hold final total.
-         for(auto x:s){
-            if(x=='('){
-                //eneter a new layer
-                st.push(0);
+         
+         int value = 0;
+         int depth = 0;
+         for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                depth ++;
             }else{
-                //child layer score reoslved. merge to parent's running score
-                int top = st.top();
-                st.pop();
-                int add = top > 0 ? 2*top : 1;
-                st.top() += add;
+                depth --;
+                if(s[i-1] == '(')
+                value +=  (1<<depth);
             }
          }
-         return st.top();
+         return value;
     }
                                                                                                                                                    
 };
