8a536e83cc951056248050a916e6b9e4c178aef8
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-03-21 10:52:27 +0530

    Time: 40 ms (46.43%), Space: 36.2 MB (46.43%) - LeetHub

--- 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-   int getMinAbsDiff(set<int>&s){
-  
-       if(s.size()==1) return 0;
-       auto it = s.begin();
+   int getMinAbsDiff(multiset<int>&ms){
+       if(ms.size()==1) return 0;
+       auto it = ms.begin();
        it++;
        int mini = INT_MAX;
-       while(it != s.end()){
+       while(it != ms.end()){
             int a = *it;
             int b = *prev(it);
-            mini = min(mini, abs(b-a));
+            if(a!=b)
+              mini = min(mini, abs(b-a));
             it++;
        }
-       return mini;
+       return mini == INT_MAX ? 0 : mini;
    }
     vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
         int m = grid.size(), n= grid[0].size();
 
         vector<vector<int>>ans(m-k+1, vector<int>(n-k+1, 0));
-        for(int i=0;i<=m-k;i++){
-            for(int j=0;j<=n-k;j++){
-                  set<int>s;  
-                  for(int p=i;p<i+k;p++){
-                     for(int q=j;q<j+k;q++){
-                          s.insert(grid[p][q]);
-                     }
-                  }
-                  ans[i][j] = getMinAbsDiff(s);
-                  s.clear();
+        for(int row = 0; row<m-k+1; row++){
+            //load first k*k submatrix
+            multiset<int>ms;
+            for(int i=row;i<row+k;i++){
+                for(int j=0;j<k;j++ ){
+                    ms.insert(grid[i][j]);
+                }
+            }
+            ans[row][0] = getMinAbsDiff(ms);
+            //sliding window
+            for(int col=k;col<n;col++){
+                //remove elements of col-k, add elements of col;
+                for(int i=row;i<row+k;i++){
+                    auto it = ms.find(grid[i][col-k]);
+                    ms.erase(it);
+                    ms.insert(grid[i][col]);
+                }
+                ans[row][col-k+1] = getMinAbsDiff(ms);
             }
         }
          
