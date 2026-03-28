2b520a56450ec31e8ebb0de8911234f99103e943
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-03-28 11:11:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -4,12 +4,12 @@ public:
     int maxDistance(vector<vector<int>>& grid) {
         int n = grid.size();
         queue<tuple<int, int>>q;
-        vector<vector<int>>vis(n, vector<int>(n, -1));
+        vector<vector<bool>>vis(n, vector<bool>(n, false));
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j] == 1){
                     q.push({i,j});
-                    vis[i][j] = 0;
+                    vis[i][j] = true;
                 }
             }
         }
@@ -23,20 +23,17 @@ public:
                 for(auto& nb:nbs){
                     int nx = i + nb.first;
                     int ny = j + nb.second;
-                    if(nx>=0 &&nx<n && ny>=0 && ny<n && vis[nx][ny]== -1){
-                        vis[nx][ny] = steps + 1;
+                    if(nx>=0 &&nx<n && ny>=0 && ny<n && !vis[nx][ny]){
+                        vis[nx][ny] = true;
                         q.push({nx, ny});
                     }
                 }
 
             }
-            steps ++;
+           if(q.size()) steps ++;
         }
 
-        int maxi = 0;
-        for(auto &x:vis){
-            for(auto&y:x) maxi = max(maxi, y);
-        }
-        return maxi ==0 ? -1 : maxi;
+        
+        return steps==0 ? -1 : steps;
     }
 };
