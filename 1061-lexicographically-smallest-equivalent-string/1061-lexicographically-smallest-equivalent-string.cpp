6b3dc67b7c18be663458ebe758ee40975c00ca37
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-03-29 21:58:24 +0530

    Time: 1 ms (51.26%), Space: 9 MB (46.22%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,50 @@
+class Dsu{
+    int n;
+    vector<int>par;
+    // vector<int>rank;
+  public:
+    Dsu(int n){
+        this->n = n;
+        par.assign(n+1, 0);
+        // rank.assign(n+1, 1);
+        for(int i=0;i<=n;i++)par[i] = i;
+    }
+
+    void _union(int i, int j){
+        int parI = findPar(i);
+        int parJ = findPar(j);
+        if(parI == parJ) return ;
+        if(parI <parJ){
+            par[j] = parI;
+        }else{
+            par[i] = parJ;
+        }
+    }
+
+    int findPar(int i){
+        if(par[i]==i) return i;
+        return par[i] = findPar(par[i]);
+    }
+};
+class Solution {
+public:
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        map<char, vector<char>>mp;
+        int n=s1.size();
+        vector<vector<int>>adj(26);
+        Dsu myDsu(26);
+        for(int i=0;i<n;i++){
+            int x = s1[i] - 'a', y = s2[i] - 'a';
+            int par1 =myDsu.findPar(x);
+            int par2 =myDsu.findPar(y);
+            myDsu._union(par1, par2);
+        }
+        string ans = "";
+        for(int i=0;i<baseStr.size(); i++){
+              int node = baseStr[i] - 'a';
+              int par = myDsu.findPar(node);
+              ans += ('a' + par);
+        }
+        return ans;
+    }
+};
