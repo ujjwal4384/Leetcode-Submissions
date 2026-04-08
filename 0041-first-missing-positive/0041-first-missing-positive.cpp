c664dce7a5e42f6ec0f29980a4cdaa09faf178b1
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-04-08 20:58:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n= nums.size();
+        int n = nums.size();
+
+        for(auto&x:nums){
+            if(x<=0) x = n+1;
+        }
         for(int i=0;i<n;i++){
-            int x = nums[i];
-            if(x<=0) nums[i] = n+1;
+             int x = abs(nums[i]);
+             if(x <=n){
+                 nums[x-1] = -1 * abs(nums[x-1]) ;
+             }
         }
 
-        for(int i=0;i<n;i++){
-            int x = nums[i];
-            if(abs(x)<=n){
-                nums[abs(x) -1] = -1 * abs(nums[abs(x) -1]); 
-            }
-        }
-
-
-        for(int i=0;i<n;i++){
-            int x = nums[i];
-            if(x >0) return i+1;
-        }
-     return n+1;
-        
+        for(int i=0;i<n;i++)if(nums[i] > 0) return i+1;
+        return n+1;
     }
 };
