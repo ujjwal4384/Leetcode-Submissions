419f1806701718d24c032ee2069096ecf0455d3f
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-04-12 22:02:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -4,10 +4,16 @@ public:
         int n = arr.size();
         //if array was empty: kth missing number =k
         //now every element we find which is <=k pushes the final answer by 1.
-        for(int i=0 ;i<n;i++){
-            if(arr[i]<=k)k++;
-            else break;
+        int l =0, r= n-1;
+        while(l<=r){
+             int m = (l+r)/2 ;
+             int missingCount =  arr[m] - (m+1);
+             if(missingCount < k)l=m+1;
+             else r=m-1;
         }
-        return k;
+        //at end: l points to exactly that element where missing count >=k
+        //hence that target sit left of l. 
+        //there are total l elements left of l hence answer pushed by l units.
+        return l+k;
     }
 };
