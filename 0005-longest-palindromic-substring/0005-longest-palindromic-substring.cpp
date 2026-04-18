8ff83d266f3e6cf3d8b4590b358c30ecac87fdfb
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-04-19 00:34:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.size();
-        bool dp[1001][1001];
-        memset(dp,0,sizeof(dp));
+        int n = s.size();
+        vector<vector<bool>>dp(n, vector<bool>(n, false));
         for(int i=0;i<n;i++){
             dp[i][i] = 1;
-            if(i+1<n&&s[i]==s[i+1])dp[i][i+1] = 1;
+            if(i+1<n && s[i] == s[i+1])dp[i][i+1] = 1; 
         }
 
-        //for 3 length to n length string
-          for(int len =3;len<=n;len++){
-             int st = 0,en=len-1;
-             while(en<n){
-                if(dp[st+1][en-1] && s[st]==s[en]){
-                    dp[st][en]=1;
-                }
-                st++,en++;
-             }
-          }
-       
-        //find longest palindromic string
-         for(int len =n;len>=1;len--){
-             
-             int st=0,en=len-1;
-             while(en<n){
-             
-                if(dp[st][en]){
-                    return s.substr(st, len);
-                }
-                st++,en++;
-             }
-         }
-         return "";           
+        for(int len = 3;len<=n; len++){
+            int st =0, en = len-1;
+            while(en<n){
+                 dp[st][en] = (s[st]==s[en]) && dp[st+1][en-1] ;
+                 st++, en++;
+            }
+        }
+
+        for(int len = n;len>=1;len--){
+            int st = 0, en = len-1;
+            while(en<n){
+               if(dp[st][en]) return s.substr(st, en-st+1);
+               st++, en++;
+            }
+        }
+        return "";
     }
 };
