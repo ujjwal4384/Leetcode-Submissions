bc1dd5eb2f345c5d8dc2cf2fe8a931583e6c0a88
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-04-19 15:57:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -17,26 +17,37 @@ public:
         }
       return lps;
     }
-    bool checkIfValid(string text, string pattern){
+    // bool checkIfValid(string text, string pattern){
         
-        string str = pattern + "#" + text;
-        vector<int> lps = kmp(str);
-        for(int i=pattern.size()+1;i<str.size(); i++){
-            if(lps[i] == pattern.size()) return true;
-        }
-        return false;
-    }
+    //     string str = pattern + "#" + text;
+    //     vector<int> lps = kmp(str);
+    //     for(int i=pattern.size()+1;i<str.size(); i++){
+    //         if(lps[i] == pattern.size()) return true;
+    //     }
+    //     return false;
+    // }
 
     int repeatedStringMatch(string a, string b) {
         
         int n = a.size();
         int m = b.size();
         string pattern = b;
-        string txt1 = a;
-        int q = 1;
-        while(txt1.size() < m)txt1 += a, q++;
-        if(checkIfValid(txt1, pattern))return q;
-        if(checkIfValid(txt1+a, pattern))return q+1;
+        vector<int>lps = kmp(pattern);
+        //simulate repeated a using modulo.  e.g a[i] -> a=[i%n];
+        // for valid b - b must start from first copy of a -> 0, n-1. 
+        int i=0, j=0;
+        while(i< n+m){
+             if(a[i%n] == b[j]){
+                    i++, j++;
+                    //match found
+                    if(j==m){
+                        return (i+n-1)/n;
+                    }
+             }else{
+                   if(j!=0) j = lps[j-1];
+                   else i++;
+             }
+        }
         return -1;
     }
 };
