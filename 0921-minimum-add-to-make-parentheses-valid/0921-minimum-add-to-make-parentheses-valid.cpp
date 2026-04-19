733c813d3cc4688dc637552166a7d06e0bff4902
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-04-19 11:10:22 +0530

    Time: 0 ms (100%), Space: 8.5 MB (57.43%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open = 0;
+        
+        int ans = 0;
+        for(auto ch: s){
+            if(ch == '('){
+                open ++;
+            }else{
+                 if(open==0)ans++;
+                 else open--;
+            }
+        }
+
+        return ans + abs(open) ;
+    }
+};
