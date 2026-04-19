2474ba074a0bffaaebc2aa9ccd344f707550c418
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-04-19 11:00:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -4,25 +4,18 @@ public:
           int n = s.size()  ;
           int ans = 0;
           for(int i=0;i<n;i++){
-            vector<int>bucket(n+1, 0);
             vector<int>freq(26, 0);
             for(int j=i; j<n ;j++){
-                       int curFreq = freq[s[j]-'a'];
-                       freq[s[j]-'a']++;
-                        if(curFreq>0){
-                            bucket[curFreq]--;
-                        }
-                        bucket[freq[s[j]-'a']]++; 
-                        int minFreq = -1, maxFreq = -1;
-                        for(int p=0;p<n+1; p++){
-                            if(bucket[p] > 0){
-                                if(minFreq==-1)minFreq = p;
-                                else 
-                                   maxFreq = p; 
+                        freq[s[j]-'a']++;
+                        int minFreq = n, maxFreq = 0;
+                        for(int k=0;k<26;k++){
+                            if(freq[k] > 0){
+                                minFreq = min(minFreq, freq[k]);
+                                maxFreq = max(maxFreq, freq[k]);
                             }
                         }
-                        if(minFreq != -1 && maxFreq != -1)
-                           ans += (maxFreq - minFreq);
+                        
+                        ans += (maxFreq - minFreq);
             }
           }
       return ans;    
