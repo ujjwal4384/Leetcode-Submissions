6a868950c63acfa2a9bac36be52b0894f305efa3
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-04-23 01:02:48 +0530

    Time: 522 ms (5.05%), Space: 97.8 MB (87.83%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    int MOD = 26;
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.size();
+        int m = shifts.size();
+        vector<int>dif(n, 0);
+        for(const auto& v: shifts){
+            int l = v[0], r = v[1], dir = v[2];
+            int val = ( (dir==1) ? 1 : -1);
+            dif[l]  = (dif[l] + val + MOD)%MOD;
+            if(r+1< n) dif[r+1] = (dif[r+1] - val + MOD)%MOD;
+        }
+        cout<<"----"<<endl;
+        for(auto x: dif) cout<<x<<" ";cout<<endl;
+        for(int i=0;i<n;i++){
+            if(i>0) dif[i] += dif[i-1];
+            int shift = ( (s[i]- 'a') + dif[i] + MOD)%MOD;
+            s[i] = 'a' + shift;
+        }
+        return s;
+    }   
+};
