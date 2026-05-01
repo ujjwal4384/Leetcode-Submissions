d1b49ec969cae43f4186ed0500328cb83f752f63
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-01 21:00:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,10 +4,10 @@ public:
        vector<int>ans;
        deque<int>dq;
        for(int i=0;i<nums.size(); i++){
-           if(i>=k && !dq.empty() && dq.back() == i-k)
-               dq.pop_back();
-           cout<< dq.front()<<" ";    
-           while(!dq.empty() && nums[dq.front()] <= nums[i] )dq.pop_front();
+           if(i>=k && !dq.empty() && dq.front() == i-k)
+               dq.pop_front();
+      
+           while(!dq.empty() && nums[dq.back()] <= nums[i] )dq.pop_back();
            dq.push_back(i);
            if(i>=k-1)ans.push_back(nums[dq.front()]);
        }
