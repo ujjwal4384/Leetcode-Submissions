3713a544f070e7b93d0a7d5de381e79b5b69c7c6
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-01 20:58:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n= nums.size();
-        vector<int>ans;
-        deque<int>dq;
-        //make sure front element is always maximum.
-        //when new guy comes and it is greater than back elements, keep poping the back elements because they can never be maximum going ahead.
-        for(int i=0;i<n;i++){
-            if(!dq.empty() && dq.front() == i-k){
-                dq.pop_front();
-            }
-            while(!dq.empty() && nums[i] >= nums[ dq.back()]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-            if(i>=k-1){
-                ans.push_back(nums[dq.front()]);
-            }
-        }
-
-      return ans;
+       vector<int>ans;
+       deque<int>dq;
+       for(int i=0;i<nums.size(); i++){
+           if(i>=k && !dq.empty() && dq.back() == i-k)
+               dq.pop_back();
+           cout<< dq.front()<<" ";    
+           while(!dq.empty() && nums[dq.front()] <= nums[i] )dq.pop_front();
+           dq.push_back(i);
+           if(i>=k-1)ans.push_back(nums[dq.front()]);
+       }
+       return ans;   
     }
 };
