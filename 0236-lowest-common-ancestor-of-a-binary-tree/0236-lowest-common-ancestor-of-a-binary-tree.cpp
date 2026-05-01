f7075214d00f19fe977e9b25c701aab3d0a40d54
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-01 13:16:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,14 +9,31 @@
  */
 class Solution {
 public:
+    TreeNode* dfs(TreeNode* root, TreeNode* p, TreeNode* q, vector<bool>&found){
+         if(!root) return NULL;
+        //for PART 2 : where nodes might be absent. 
+        
+        TreeNode* lNode = dfs(root->left, p, q, found);
+        TreeNode* rNode = dfs(root->right, p, q, found);
+        if(root == p){
+             found[0] = true;
+             return root;
+        }
+        else if(root == q){
+             found[1] = true;
+             return root;
+        }
+
+        if(lNode  && rNode) return root;
+        return lNode ? lNode : rNode;
+
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(p == q) return p;
         if(!root) return NULL;
-        if(root == p || root == q) return root;
-       TreeNode* lNode = lowestCommonAncestor(root->left, p, q);
-       TreeNode* rNode = lowestCommonAncestor(root->right, p, q);
-        
-        if(lNode  && rNode) return root;
-      return lNode ? lNode : rNode;
+        //for PART 2 : where nodes might be absent. 
+        vector<bool>found(2, false);
+       TreeNode* lca = dfs(root, p, q, found);
+      return found[0] && found[1] ? lca : NULL;
     }
 };
