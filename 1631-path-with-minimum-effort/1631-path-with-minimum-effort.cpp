3d0ccb5d730be95ad1214f72ac7358efdf372a00
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-02 18:41:30 +0530

    Time: 38 ms (93.74%), Space: 24.6 MB (56.62%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,5 +1,6 @@
 class Solution {
     pair<int,int>nbs[4] = {{-1,0},{1,0},{0,-1},{0,1}};
+    int INF = 1e8;
 public:
     bool isPossible(int i, int j, vector<vector<int>>& heights, vector<vector<bool>>&vis, int& mid){
         vis[i][j] = true;
@@ -18,19 +19,29 @@ public:
     }
     int minimumEffortPath(vector<vector<int>>& heights) {
         int m = heights.size(), n= heights[0].size(); 
-        int ans = 1e6;
-        int l =0, r = 1e6;
-        while(l<=r){
-            int mid = l +((r-l)>>1);
-            vector<vector<bool>>vis(m, vector<bool>(n, false));
-            if(isPossible(0, 0, heights, vis, mid)){
-                 ans = mid;
-                 r = mid-1;
-            }else{
-                l= mid+1;
-            }
-        }
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
+        vector<vector<int>>dis(m, vector<int>(n, INF));
+        dis[0][0] = 0;
+        pq.push({0, {0,0}});
+        while(!pq.empty()){
+                auto [cost, pos] = pq.top();
+                auto [i, j] = pos;
+                pq.pop();
+                if(i == m-1 && j == n-1) return cost;
+                for(auto &nb: nbs){
+                    int nx = i+nb.first, ny = j+nb.second;
+                    if(nx>=0 && nx< m && ny>=0 && ny< n){
+                        int edgeWeight = abs(heights[nx][ny] - heights[i][j]);
+                        int newCost = max(cost, edgeWeight);
+                        if(dis[nx][ny] > newCost){
+                            dis[nx][ny] = newCost;
+                            pq.push({dis[nx][ny], {nx, ny}});
+                        } 
+                        
+                    }
+                }
+        }    
 
-        return ans;
+        return 0;
     }
 };
