c38e1a5d371fb355f3b7ea1c18697cb64ba1c274
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-09 17:49:18 +0530

    Time: 0 ms (100%), Space: 69.7 MB (25.67%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-         int n = nums.size();
-         int l=0; int max_streak = k;
-         int debt = 0;
-         for(int r=0;r<n;r++){
-                if(nums[r] == 0)debt++;
-                while(debt>k){
-                    if(nums[l] == 0)debt--; //refund
-                    l++;
-                }
-                max_streak = max(max_streak, r-l+1);
-         }
+         int left = 0, max_streak = 0, debt = 0;
 
-         return max_streak;
+        // You running down the aisle
+        for (int right = 0; right < nums.size(); right++) {
+            
+            // You swiped your card on a premium item
+            if (nums[right] == 0) debt++; 
+            
+            // Card declined! Accountant starts putting things back
+            while (debt > k) {
+                if (nums[left] == 0) {
+                    debt--; // Refund!
+                }
+                left++; // Accountant moves forward
+            }
+            
+            // Cart is valid, check the size
+            max_streak = max(max_streak, right - left + 1);
+        }
+        return max_streak;
     }
 };
