16ecb54f95c73164b302ae9bde5ec7538d122a30
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-09 17:20:38 +0530

    Time: 139 ms (29.72%), Space: 69.3 MB (26.21%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -3,15 +3,15 @@ class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
         //{val ,{row,col}}
-        multiset< ppi >ms;
+        set< ppi >s;
         int m = nums.size(), n = nums[0].size();
         for(int i=0;i<m;i++){
-            ms.insert({nums[i][0], {i,0}});
+            s.insert({nums[i][0], {i,0}});
         }
         int ans_mini = 0, ans_maxi = INT_MAX; 
-        while(ms.size() == m){
-                auto it1 =  ms.begin();
-                auto it2 =  --ms.end();
+        while(s.size() == m){
+                auto it1 =  s.begin();
+                auto it2 =  --s.end();
                 
                 auto [mini, cord1] = *it1;
                 auto [maxi, cord2] = *it2;
@@ -22,10 +22,10 @@ public:
                     ans_maxi = maxi;
                 }
 
-                ms.erase(it1);
+                s.erase(it1);
                 if(j1< nums[i1].size() -1 ){
                      
-                    ms.insert({ nums[i1][j1+1] ,{i1, j1+1}});
+                    s.insert({ nums[i1][j1+1] ,{i1, j1+1}});
                 }
 
         }
