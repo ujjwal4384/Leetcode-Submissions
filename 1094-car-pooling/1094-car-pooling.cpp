155bc47d483a4be224aa8b05b3691fe42de72915
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-19 20:06:45 +0530

    Time: 0 ms (100%), Space: 14.5 MB (39.68%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int>diff(10001, 0);
+        // vector<int>diff(10001, 0);
+        map<int,int>diff;
         for(auto& trip:trips){
                  int l = trip[1], r= trip[2], val = trip[0];
                  diff[l] += val;
                  diff[r] -= val;
         }
-
-        for(int i=0;i<=1000; i++){
-            if(i>0) diff[i] += diff[i-1];
-            if(diff[i] > capacity) return false;
+        int curCap = 0;
+        for(auto& p:diff){
+            curCap += p.second; 
+            if(curCap > capacity) return false;
         }
         return true;
     }
