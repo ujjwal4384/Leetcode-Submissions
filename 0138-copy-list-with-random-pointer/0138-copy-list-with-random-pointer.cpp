6555dac69267d41ba3a3ef59e618574d2180b18e
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-19 20:25:52 +0530

    Time: 11 ms (14.75%), Space: 15.3 MB (26.72%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,42 +17,26 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-       if(!head) return NULL;
+        unordered_map<Node*, Node*>oldToNewNode;
+        Node* temp = head;
+        //create and assign next pointer
+        Node* newPrev = NULL;
+        while(temp){
+            oldToNewNode[temp] = new Node(temp->val);
+            if(newPrev)
+                 newPrev->next = oldToNewNode[temp];
+            newPrev = oldToNewNode[temp];
+            temp = temp->next;
+           
+        }
+        
 
-       Node* temp = head;
-
-      
-        // step-1: interweave. add new node in middle of two old nodes.
-       while(temp){
-        Node* newNode = new Node(temp->val);
-        newNode->next = temp->next;
-        temp->next = newNode;
-        temp = temp->next->next;
-       } 
-
-       // step-2: assign random pointer for new nodes
-       temp = head; 
-       while(temp){
-             temp->next->random = temp->random ? temp->random->next: NULL;
-             temp = temp->next->next;   
-       } 
-       
-      //step-3: separate new node list from old.  
-      Node* newHead = head->next;
-      temp = head;
-      while(temp){
-            Node* curOldNode = temp;
-            Node* nextOldNode = temp->next->next;
-
-            Node* curNewNode = temp->next;
-            Node* nextNewNode = nextOldNode ? nextOldNode->next : NULL;
-
-            curOldNode->next = nextOldNode;
-            curNewNode->next = nextNewNode;
-
-            temp = nextOldNode;
-      }
-       
-      return newHead;
+        //assign random pointer
+        temp = head;
+        while(temp){
+            oldToNewNode[temp]->random =  oldToNewNode[temp->random];
+            temp = temp->next;
+        }
+        return oldToNewNode[head];
     }
 };
