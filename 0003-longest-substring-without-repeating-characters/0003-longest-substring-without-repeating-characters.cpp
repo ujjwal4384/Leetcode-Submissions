ce40438bcc03c1c8b62815a4b5cd7aad3d2a6873
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-22 00:41:45 +0530

    Time: 1 ms (85.47%), Space: 11.4 MB (86.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //sliding window of dynamic size
-
-        int n=s.size();
-        if(n<=1) return n; 
-
-        int st =0, en =1;
-
-        map<char, int>mp;
-        //character currently getting iterated should have occurence to be 0 , if it's 1 that means it is already present in this window, so, forward the st pointer till you find that character, and also make occurences of all those forwared st chacters to 0 because now they are available to be taken in new window. 
-        mp[s[0]]=1;
-        int maxi =1;
-        while(en<n){
-            //  cout<<st<<" "<<en<<"    #  mp[s[en]] = "<<mp[s[en]]<<endl;
-             
-         if(!mp[s[en]]){
-             mp[s[en]] = 1;
-             maxi = max(maxi, en-st +1);
-             en++;
-         }else{
-             while(s[st] !=s[en]){
-                 mp[s[st]] =0;
-                 st++;
-             }
-             //do not make mp[en] =0 because it is part of updated window. 
-             st++;
-             en++;
-         }
-         
+        int n = s.size();
+        if(n<=1) return n;
+        int ans = 1;
+        int freq[256] = {0};
+        int l=0, r=0;
+        while(r<n){
+            freq[s[r]]++;
+            while(freq[s[r]] > 1){
+                    freq[s[l]]--;
+                    l++; 
+            }
+            ans = max(ans, r-l+1);
+            r++;
         }
-       return maxi; 
+     return ans;   
     }
 };
