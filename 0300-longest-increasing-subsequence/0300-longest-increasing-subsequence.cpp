a8b3a53a024abfb47988d2661f6877e7851c0ddb
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-23 16:41:30 +0530

    Time: 0 ms (100%), Space: 14.1 MB (74.6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n, 1);
-        int maxi=1;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                    if(nums[j] < nums[i]) 
-                       dp[i] = max(dp[i], dp[j]+1);
-            }
-            maxi = max(maxi, dp[i]);
+        vector<int>tail;
+        //tail[i]= stores last smallest element for subsequenece of length i+1.
+        //it does not store correct subsequence, just helpful for length.
+        for(int i=0;i<n;i++){
+                auto it = lower_bound(tail.begin(), tail.end(), nums[i]);
+                if(it == tail.end()){
+                    tail.push_back(nums[i]);
+                    continue;
+                }
+                //overwrite that index with this value
+                *it = nums[i];
         }
-        return maxi;
+
+        return tail.size();
     }
 };
+/*
+
+ 1 7 8 4 9
+
+ 1->[1]
+ 7->[1, 7]
+ 8->[1, 7, 8]
+ 4 -> not greater than end of tail hence find just greater eq element and overwrite that
+    -> [1, 4, 8] //Note 1 4 8 is not correct subsequence as 4 comes after 8 in array but it gives correct LIS length because we know if 8 is there there was some element smaller than 8 (7) before. not concerned on what that element was. 
+5-> [1,4,8, 9] ans = 4 .tail.size()
+*/
