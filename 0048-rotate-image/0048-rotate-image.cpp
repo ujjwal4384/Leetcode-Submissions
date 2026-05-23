2db7751f2edf0dab5736a8fec4c61ab87eb408b6
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-23 15:20:26 +0530

    Time: 0 ms (100%), Space: 10.2 MB (67.69%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,11 +2,31 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
      int n = matrix.size();
-     for(int i= 0; i<n; i++){
-        for(int j=i+1; j<n; j++){
-            swap(matrix[i][j], matrix[j][i]);
+     //solving in layers, 4 way swap
+     //mirrorImage[j][n-1-i] = matrix[i][j]
+     /*
+        old_row = i old_col = j
+        new_row=j   new_col = n-1-j appl;y this four times to get top right bottom right bottom left coordinates
+     */
+
+     /*
+     i defines layers. 
+     and j traverses the top row of that layer
+      becasue on every top-left element: it's corresponding top-right, bottom-right and bottom-left are also getting placed correctly.
+     */
+     for(int i= 0; i<n/2; i++){
+        for(int j=i;j< n-1-i; j++){
+             int top_left = matrix[i][j];
+             int top_right = matrix[j][n-1-i] ;//top-right
+             int bottom_right = matrix[n-1-i][n-1-j]; //bottom-right
+             int bottom_left = matrix[n-1-j][i]; //bottom-left
+             
+             
+             matrix[j][n-1-i] = top_left;
+             matrix[n-1-i][n-1-j] = top_right;
+             matrix[n-1-j][i] = bottom_right;
+             matrix[i][j] = bottom_left;
         }
-        reverse(matrix[i].begin(), matrix[i].end());
      }
     }
 };
