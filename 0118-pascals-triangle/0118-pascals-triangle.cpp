3058de34ac0b2b4f4b4dc98d483035ac6c7b4468
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-23 14:15:28 +0530

    Time: 0 ms (100%), Space: 9.8 MB (10.57%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -6,11 +6,11 @@ public:
         for(int row = 2; row<=numRows; row++){
              int sz = v.back().size();
              vector<int>lastRow = v.back();
-             vector<int>newRow = {1};
+             vector<int>newRow(sz+1, 1);
              for(int j=1; j<sz; j++){
-                 newRow.push_back(lastRow[j] + lastRow[j-1]);
+                 newRow[j] = (lastRow[j] + lastRow[j-1]);
              }
-             newRow.push_back(1);
+             
              v.push_back(newRow);
         }
         return v;
