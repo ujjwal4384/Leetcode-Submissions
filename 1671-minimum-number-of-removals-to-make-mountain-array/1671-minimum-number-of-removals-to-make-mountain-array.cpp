4da4c9e23c50fb4a80fc6309f91fdca0cb1979c9
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-24 02:06:01 +0530

    Time: 4 ms (85.1%), Space: 16.8 MB (7.02%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,22 +1,31 @@
 class Solution {
+
+    vector<int> getLISLengths(const vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        vector<int> tails;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = lower_bound(tails.begin(), tails.end(), nums[i]);
+            if (it == tails.end()) {
+                tails.push_back(nums[i]);
+                dp[i] = tails.size(); // The length is the new size of the tails array
+            } else {
+                *it = nums[i];
+                dp[i] = distance(tails.begin(), it) + 1; // The length is its position + 1
+            }
+        }
+        return dp;
+    }
+
 public:
     int minimumMountainRemovals(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp1(n, 1), dp2(n, 1);
-        for(int i=0;i<n;i++){
-            for(int j=0; j<i; j++){
-                if(nums[j] < nums[i])
-                    dp1[i] = max(dp1[i], dp1[j] + 1);
-            }
-        }
-
-        for(int i=n-1;i>=0;i--){
-            for(int j=n-1; j>=i+1; j--){
-                if(nums[j] < nums[i])
-                    dp2[i] = max(dp2[i], dp2[j] + 1);
-            }
-        }
-
+        dp1 = getLISLengths(nums);
+        reverse(nums.begin(), nums.end());
+        dp2 = getLISLengths(nums);
+        reverse(dp2.begin(), dp2.end()) ;
+        
         int mini = n;
         for(int i=1;i<n-1;i++){
             //if nums[i] is mountain array then
