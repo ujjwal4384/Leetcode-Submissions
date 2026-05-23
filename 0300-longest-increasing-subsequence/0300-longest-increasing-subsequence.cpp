2cf57c5d06e55065ec0957a500fee5adcbb82cb7
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-23 17:09:01 +0530

    Time: 12 ms (71%), Space: 14.6 MB (34.97%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,16 +5,34 @@ public:
         vector<int>tail;
         //tail[i]= stores last smallest element for subsequenece of length i+1.
         //it does not store correct subsequence, just helpful for length.
+        vector<int>par(n, -1);
+        vector<int>tail_indices;
         for(int i=0;i<n;i++){
-                auto it = lower_bound(tail.begin(), tail.end(), nums[i]);
-                if(it == tail.end()){
+                
+                int pos = lower_bound(tail.begin(), tail.end(), nums[i]) - tail.begin();
+                if(pos>0){
+                    par[pos] = tail_indices[pos-1];
+                }
+                if(pos == tail.size()){
                     tail.push_back(nums[i]);
+                    tail_indices.push_back(i);
                     continue;
                 }
                 //overwrite that index with this value
-                *it = nums[i];
+                
+                tail[pos] = nums[i];
+                tail_indices[pos] = i;
         }
-
+        vector<int>ans;
+        int cur = tail_indices.back();
+        while(cur != -1){
+            ans.push_back(nums[cur]);
+            cur = par[cur];
+        }
+        reverse(ans.begin(), ans.end());
+        for(auto&x:ans){
+            cout<<x<<" ";
+        }    
         return tail.size();
     }
 };
@@ -28,4 +46,7 @@ public:
  4 -> not greater than end of tail hence find just greater eq element and overwrite that
     -> [1, 4, 8] //Note 1 4 8 is not correct subsequence as 4 comes after 8 in array but it gives correct LIS length because we know if 8 is there there was some element smaller than 8 (7) before. not concerned on what that element was. 
 5-> [1,4,8, 9] ans = 4 .tail.size()
+
+Now what if you want longest subequence also in O(nLogn)?
+Then need to store parent array info.
 */
