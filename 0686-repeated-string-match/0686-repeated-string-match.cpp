5780b7ef9f1df0abb502dfbc6f792384e1f7cdc1
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-24 16:45:16 +0530

    Time: 0 ms (100%), Space: 9.8 MB (43.82%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,52 +1,44 @@
 class Solution {
-public:
-    vector<int> kmp(string& s){
+    vector<int> kmp(string&s){
         int n = s.size();
-        vector<int>lps(n, 0);
-        int i=1;
-        int len = 0;
+        vector<int>lps(n,0);
+
+        int i=1,len=0;
         while(i<n){
              if(s[i] == s[len]){
                 len++;
                 lps[i] = len;
                 i++;
              }else{
-                  if(len!=0) len = lps[len-1];
-                  else i++;
+                if(len!=0)len = lps[len-1];
+                else i++;
              }
         }
-      return lps;
+     return lps;   
     }
-    // bool checkIfValid(string text, string pattern){
-        
-    //     string str = pattern + "#" + text;
-    //     vector<int> lps = kmp(str);
-    //     for(int i=pattern.size()+1;i<str.size(); i++){
-    //         if(lps[i] == pattern.size()) return true;
-    //     }
-    //     return false;
-    // }
-
+public:
     int repeatedStringMatch(string a, string b) {
-        
-        int n = a.size();
-        int m = b.size();
+        int n = a.size(), m = b.size();
         string pattern = b;
         vector<int>lps = kmp(pattern);
-        //simulate repeated a using modulo.  e.g a[i] -> a=[i%n];
-        // for valid b - b must start from first copy of a -> 0, n-1. 
-        int i=0, j=0;
-        while(i< n+m){
-             if(a[i%n] == b[j]){
-                    i++, j++;
-                    //match found
-                    if(j==m){
-                        return (i+n-1)/n;
-                    }
-             }else{
-                   if(j!=0) j = lps[j-1];
-                   else i++;
-             }
+        //how many copies of a will be needed at max? b length / a length ceil. 
+        //instead of creating multiple copies of a to iterate- use modulo for cicular iteration/
+        //if there is possbility- then b's first char should match a in first copy only. in worsrt case at last char of a = b ka first character.
+        //after that j will go m  characters. therefore i till> n-1 + m. if no match found until there also, then not posssible.
+
+        int i=0,j=0;
+        while(i<m+n){
+                if(a[i%n] == b[j]){
+                     i++, j++;
+                     //match found
+                     if(j==m){
+                        //calculate number of copies of a=> total chars of a used and length of one copy
+                        return ceil((0.0+i)/n);
+                     }
+                }else{
+                    if(j!= 0) j = lps[j-1];
+                    else i++;
+                }    
         }
         return -1;
     }
