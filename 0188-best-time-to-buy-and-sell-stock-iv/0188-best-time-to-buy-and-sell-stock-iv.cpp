29c6e7c67b38f36eb3041b52a4e82429d7da61ed
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-26 03:29:22 +0530

    Time: 3 ms (80.32%), Space: 15.1 MB (69.01%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,44 +1,33 @@
 class Solution {
-public:
-    int f(vector<int>& prices, int i, int type, int tx,int dp[][2][101] ){
-        //BASE CASE
-        if(tx==0){
-            return 0;
-        }
-        else if(i==prices.size()-1){
-            if(type==0){
-                //donot buy
-                return 0;
-            }else{
-                //must sell.
+    int f(int i, int tx, bool canBuy, vector<int>& prices, int dp[1001][101][2]){
+        if(tx==0) return 0;
+    
+        if(i==prices.size()-1){
+            if(!canBuy)
                 return prices.back();
-            }
+            return 0;    
         }
-        else if(i>=prices.size()){
-            return INT_MIN;
+        
+        if(dp[i][tx][canBuy] != -1) return dp[i][tx][canBuy];
+        if(canBuy){
+            int actualyBuy = tx>0 ? (-prices[i] + f(i+1, tx, 0, prices, dp) ) : 0;
+            int skipsBuying = f(i+1, tx, 1, prices, dp);
+            return dp[i][tx][canBuy] = max(actualyBuy, skipsBuying);
+        }else{
+            
+            int actualySell = prices[i] + f(i+1, tx-1, 1, prices, dp);
+            int skipsSelling = f(i+1, tx, 0, prices, dp);
+            return dp[i][tx][canBuy] = max(actualySell, skipsSelling);
         }
-
-        else if(dp[i][type][tx]!=-1) return dp[i][type][tx];
-        // BUY
-        if(type==0){
-            int notBuy= INT_MIN, buy= INT_MIN;
-            notBuy = f(prices, i+1, type, tx, dp);
-            buy = -1* prices[i] + f(prices, i+1, 1-type, tx, dp);
-            return dp[i][type][tx]= max(notBuy, buy);
-        }
-        //SELL
-        else if(type==1){
-            int notSell= INT_MIN, sell=INT_MIN;
-            notSell = f(prices, i+1, type, tx, dp);
-            sell = prices[i] + f(prices, i+1, 1-type, tx-1, dp);        
-            return dp[i][type][tx]= max(sell, notSell);
-        }
-      return INT_MIN;
+        return 0;
     }
+public:
     int maxProfit(int k, vector<int>& prices) {
-        int dp[1001][2][101];
+        int n = prices.size();
+        int tx = k;
+        bool canBuy = true;
+        int dp[1001][101][2];
         memset(dp, -1, sizeof(dp));
-
-      return max(0, f(prices, 0 , 0, k, dp));
+        return max(0, f(0, tx, canBuy, prices, dp));
     }
 };
