a0e8b0eeba4c39c5567d752bf495a010d22446b0
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-25 14:19:16 +0530

    Time: 0 ms (100%), Space: 8 MB (87.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,14 @@
 class Solution {
-public:
     int f(int i, int j, int dp[101][101]){
-        if(i==0 && j==0){
-            return 1;
-        }
-        else if(i<0 || j<0){
-            return 0;
-        }
+        if(i==0 && j==0) return 1;
+        else if(i<0 || j<0) return 0;
         else if(dp[i][j] != -1) return dp[i][j];
-
-        int fromTop = 0,fromLeft=0;
-        fromTop = f(i-1, j, dp);
-        fromLeft = f(i,j-1, dp);
-
-        return dp[i][j] =  fromTop + fromLeft;
+        return dp[i][j] = f(i-1, j, dp) + f(i, j-1, dp);
     }
+public:
     int uniquePaths(int m, int n) {
         int dp[101][101];
         memset(dp, -1, sizeof(dp));
-        return f(m-1, n-1, dp);
+        return f(m-1,n-1, dp);
     }
 };
