89e16756a1ea0c0658a4f04ab081d45c20afb0fd
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-25 12:37:49 +0530

    Time: 3 ms (6.17%), Space: 11 MB (20.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,56 +1,18 @@
 class Solution {
-public:
-    int f(int i, int canPick, vector<int>& nums, int dp[101][2]){
-        if(i>=nums.size()) return 0;
-        else if(dp[i][canPick] !=-1) return dp[i][canPick];
-
-
-        if(canPick){
-            // actually picks, toggle nes=xt indexes state
-            int a = nums[i] + f(i+2, canPick, nums, dp);
-            // skips
-            int b = f(i+1, canPick, nums, dp);
-            return dp[i][canPick] = max(a, b);
-        }else{
-            // can't pick, toggle next indexes state of pick.
-             return dp[i][canPick] =f(i+1, !canPick, nums, dp);   
+    int solve(int i, int canTake, vector<int>& nums, vector<vector<int>>&dp){
+        if(i>= nums.size()) return 0;
+        if(dp[i][canTake] != -1) return dp[i][canTake];
+        int notTake = solve(i+1, 1, nums, dp);
+        int take = INT_MIN;
+        if(canTake){
+            take = nums[i] + solve(i+1, 0, nums, dp);
         }
-        return 0;
+        return dp[i][canTake] = max(take, notTake);
     }
-
-    //   vector<int>cur(2,0), nxt(2, 0);
-    //      for(int i= n-1; i>=0; i--){
-    //         for(int take =0 ; take<=1; take++){
-    //              if(take){
-    //                  cur[take] = nums[i] +  nxt[0] ;
-    //              }
-    //              else{ 
-    //                  cur[take] = max(nxt[1], nxt[0]);
-    //              }
-    //         }
-    //         nxt = cur;
-    //      }
-
-    //      return max(nxt[1], nxt[0]);
-
+public:
     int rob(vector<int>& nums) {
-        int dp[103][2];
-        memset(dp, 0, sizeof(dp));
-        vector<int>cur(2,0),nxt(2,0);
-        for(int i=nums.size()-1;i>=0;i--){
-            for(int canTake = 0;canTake<=1;canTake++){
-                if(canTake){
-                    int a = nums[i] + nxt[!canTake];
-                    int b = nxt[canTake];
-                    cur[canTake] = max(a, b);
-                }else{
-                    cur[canTake] = nxt[!canTake];   
-                }
-                 
-            }
-            nxt = cur;
-        }
-
-        return cur[1];
+        int n = nums.size();
+        vector<vector<int>>dp(n, vector<int>(2,-1));
+        return solve(0, 1, nums, dp);
     }
 };
