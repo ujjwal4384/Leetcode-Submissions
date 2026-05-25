8ef9635c5c97f62ffe610ced5723cbd18df292ef
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-26 03:27:14 +0530

    Time: 51 ms (61.4%), Space: 91.2 MB (55.49%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,21 @@
 class Solution {
     int f(int i, int tx, bool canBuy, vector<int>& prices, int dp[100001][3][2]){
-        if(tx<0) return 0;
-        if(i>=prices.size()){
-            return 0;
+        if(tx==0) return 0;
+    
+        if(i==prices.size()-1){
+            if(!canBuy)
+                return prices.back();
+            return 0;    
         }
-        if(i==prices.size()-1 && !canBuy){
-            return prices.back();
-        }
+        
         if(dp[i][tx][canBuy] != -1) return dp[i][tx][canBuy];
         if(canBuy){
-            int actualyBuy = tx>0 ? (-prices[i] + f(i+1, tx-1, 0, prices, dp) ) : 0;
+            int actualyBuy = tx>0 ? (-prices[i] + f(i+1, tx, 0, prices, dp) ) : 0;
             int skipsBuying = f(i+1, tx, 1, prices, dp);
             return dp[i][tx][canBuy] = max(actualyBuy, skipsBuying);
         }else{
             
-            int actualySell = prices[i] + f(i+1, tx, 1, prices, dp);
+            int actualySell = prices[i] + f(i+1, tx-1, 1, prices, dp);
             int skipsSelling = f(i+1, tx, 0, prices, dp);
             return dp[i][tx][canBuy] = max(actualySell, skipsSelling);
         }
