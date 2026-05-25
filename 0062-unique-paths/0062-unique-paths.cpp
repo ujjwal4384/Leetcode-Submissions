f13466fc58389e83ed6058616fc64b779180f1bd
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-25 14:35:57 +0530

    Time: 0 ms (100%), Space: 7.9 MB (92.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -7,8 +7,21 @@ class Solution {
     }
 public:
     int uniquePaths(int m, int n) {
-        int dp[101][101];
-        memset(dp, -1, sizeof(dp));
-        return f(m-1,n-1, dp);
+        if (m < n) swap(m, n);
+        
+        if(m==1 || n==1) return 1;
+        //total m-1 c+ n-1 steps to reach from top-left to bottom-right
+        //out of which m-1 will be in down directioj and n-1 wil be in right direfction
+        // = ways = (m+n-2) C (m-1)
+
+        // (m+n-2)!/ (m-1)!(n-1)!   = (m+n-2)(m+n-1)....(m) / (n-1)(n-2)...(1)
+
+
+        long long ans = 1;
+        for(int i=1;i<=n-1;i++){
+            ans =   ans * (m+n-i-1) / i ;
+        }
+        return (int)ans;
     }
 };
+        
