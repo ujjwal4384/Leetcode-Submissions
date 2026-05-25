ff452249376b74053e5a71ab1c933a0fd7fea163
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-25 18:37:27 +0530

    Time: 3 ms (85.23%), Space: 11.9 MB (73.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,13 +1,33 @@
 class Solution {
-    int f(int i, vector<int>& nums, int target, int cur){
-        if(i>=nums.size()) return cur==target;
+    int f(int i, vector<int>& nums, int target, int dp[21][2001]){
+        if(target<0 || i>nums.size()) return 0;
+        if(i==nums.size())return target==0;
         
-        return f(i+1, nums, target, cur+nums[i]) + f(i+1, nums, target, cur-nums[i]);
+        if(dp[i][target] != -1) return dp[i][target];
         
+        return  dp[i][target] = f(i+1, nums, target - nums[i], dp) + f(i+1, nums, target, dp);      
     }
+
+
+
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int cur = 0;
-        return f(0, nums, target, cur);
+        //look all numbers are positive. 
+        //original problem -> wayas to build target. it can be negative also. so inorder to aovid using map for dp.
+        //reframe problem- P =numbers having positive sign, N = numbers with negative sign/ 
+        //P-N = target, also P + N = total_sum. Hence add 2 equation-> p = (target + sum)/2. this will always be >=0 
+        //Becasue if target < sum => not possible only becasue max we can get is [-sum, +sum].
+        //hence now we just weant subsets with total = k= (target+sum)/2 ;
+
+        
+        int total_sum = 0;
+        for(auto& x: nums) total_sum += x;
+        if(abs(target) > total_sum) return 0;
+        int dp[21][2001];
+        memset(dp, -1, sizeof(dp));
+        int p = (target + total_sum);
+        if(p%2) return 0;
+        p /= 2;    
+        return f(0, nums, p, dp);
     }
 };
