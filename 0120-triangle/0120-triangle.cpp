74b18a9f3efeacfa7f0c8e1345eed137bb94d3a8
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-25 16:28:22 +0530

    Time: 0 ms (100%), Space: 13.3 MB (51.33%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,51 +1,16 @@
 class Solution {
 public:
-    int f(int row,int col, vector<vector<int>>& triangle, int dp[201][201]){
-        if(row==0 && col==0)return triangle[0][0];
-        if(col>row ||col<0)return INT_MAX;
-        if(dp[row][col]!=-1)return dp[row][col];
-        int result=INT_MAX;
-        int way1 = f(row-1, col, triangle, dp);
-        int way2= f(row-1,col-1, triangle, dp);
-        result=min(way1,way2)+triangle[row][col];
-        return dp[row][col] = result;
-        
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-//         int row=triangle.size();
-      
-//         int dp[201][201];
-//         memset(dp,0,sizeof(dp));
-//         dp[0][0] = triangle[0][0];
-//         for(int i=1;i<row;i++){
-//             for(int j=0;j<=i;j++){
-//                 int way1 = INT_MAX,way2=INT_MAX;
-//                 if(j<=i-1)way1=dp[i-1][j];
-//                 if(j-1>=0)way2 = dp[i-1][j-1];
-//                 dp[i][j] = min(way1, way2) + triangle[i][j];
-//             }
-//         }
-//         int ans=INT_MAX;
-//         for(int j=0;j<triangle.size();j++){
-//             ans= min(ans, dp[triangle.size()-1][j]);
-//         }
-        
-        vector<int>pre(1);
-        pre[0]=triangle[0][0];
-         for(int i=1;i<triangle.size();i++){
-            vector<int>cur(pre.size()+1);
-            for(int j=0;j<=i;j++){
-                int way1 = INT_MAX,way2=INT_MAX;
-                if(j<=i-1)way1=pre[j];
-                if(j-1>=0)way2 = pre[j-1];
-                cur[j] = min(way1, way2) + triangle[i][j];
+        int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n, 0));
+        dp[0][0] = triangle[0][0];
+        for(int row=1; row<n; row++){
+            for(int col = 0; col<=row; col++){
+                  int a = col <=row-1 ? dp[row-1][col] : INT_MAX;
+                  int b = col-1 >=0 ? dp[row-1][col-1] : INT_MAX;
+                  dp[row][col] = triangle[row][col] +  min(a, b);
             }
-             pre=cur;
         }
-        int ans=INT_MAX;
-        for(int j=0;j<pre.size();j++){
-            ans= min(ans, pre[j]);
-        }
-       return ans; 
+      return *min_element(dp[n-1].begin(), dp[n-1].end());  
     }
 };
