deb9c6b5f70b39f98deb1936eeb3995de207ff5e
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-25 12:44:14 +0530

    Time: 0 ms (100%), Space: 10.2 MB (75.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,30 @@
 class Solution {
-    int solve(int i, int canTake, vector<int>& nums, vector<vector<int>>&dp){
-        if(i>= nums.size()) return 0;
-        if(dp[i][canTake] != -1) return dp[i][canTake];
-        int notTake = solve(i+1, 1, nums, dp);
-        int take = INT_MIN;
-        if(canTake){
-            take = nums[i] + solve(i+1, 0, nums, dp);
-        }
-        return dp[i][canTake] = max(take, notTake);
-    }
+    // int solve(int i, int canTake, vector<int>& nums, vector<vector<int>>&dp){
+    //     if(i>= nums.size()) return 0;
+    //     if(dp[i][canTake] != -1) return dp[i][canTake];
+    //     int notTake = solve(i+1, 1, nums, dp);
+    //     int take = INT_MIN;
+    //     if(canTake){
+    //         take = nums[i] + solve(i+1, 0, nums, dp);
+    //     }
+    //     return dp[i][canTake] = max(take, notTake);
+    // }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(2,0));
+        
+        vector<int>next(2,0), cur(2,0);
         for(int i=n-1;i>=0;i--){
             for(int canTake = 0; canTake<=1; canTake++){
-                        int notTake = dp[i+1][1];
+                        int notTake = next[1];
                         int take = INT_MIN;
                         if(canTake){
-                            take = nums[i] + dp[i+1][0];
+                            take = nums[i] + next[0];
                         }
-                        dp[i][canTake] = max(take, notTake);
+                        cur[canTake] = max(take, notTake);
             }
+            next = cur;
         }
-        return dp[0][1];
+        return cur[1];
     }
 };
