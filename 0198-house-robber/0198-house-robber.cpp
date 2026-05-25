0cc3a78d1e2557a3ef43f9bd9924ce33421a5e88
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-25 12:42:36 +0530

    Time: 3 ms (6.17%), Space: 11 MB (20.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,7 +12,17 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n, vector<int>(2,-1));
-        return solve(0, 1, nums, dp);
+        vector<vector<int>>dp(n+1, vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int canTake = 0; canTake<=1; canTake++){
+                        int notTake = dp[i+1][1];
+                        int take = INT_MIN;
+                        if(canTake){
+                            take = nums[i] + dp[i+1][0];
+                        }
+                        dp[i][canTake] = max(take, notTake);
+            }
+        }
+        return dp[0][1];
     }
 };
