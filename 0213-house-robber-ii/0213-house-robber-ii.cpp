9fe3d22531be70019871523310f7668fd7b4d43c
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-25 12:53:55 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.51%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,26 +1,31 @@
 class Solution {
-
-    int helper(vector<int>& nums, int l, int r) {
-        int n = nums.size();
-        
-        vector<int>next(2,0), cur(2,0);
-        for(int i=r;i>=l;i--){
-            for(int canTake = 0; canTake<=1; canTake++){
-                        int notTake = next[1];
-                        int take = INT_MIN;
-                        if(canTake){
-                            take = nums[i] + next[0];
-                        }
-                        cur[canTake] = max(take, notTake);
-            }
-            next = cur;
-        }
-        return cur[1];
-    }
 public:
+    int f(int i, int take, vector<int>& nums, int dp[101][2], int closing){
+        if(i>=closing){
+            return 0;
+        }
+        
+       if(dp[i][take] !=-1) return dp[i][take];
+
+        if(take){
+            return dp[i][take] = nums[i] +  f(i+1, 0, nums, dp, closing) ;
+        }
+        else{ 
+            return dp[i][take] = max(f(i+1, 1, nums, dp, closing), f(i+1, 0, nums, dp, closing));
+        }
+
+        return -1;
+    } 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums.front();
-         return max(helper(nums, 0, n-2), helper(nums, 1, n-1));
+        if(n==1) return nums[0];
+        int dp[101][2];
+        memset(dp, -1, sizeof(dp));
+        
+        int pickedHouse1 = f(0, 1, nums, dp, n-1);
+        memset(dp, -1, sizeof(dp));
+        int notPickedHouse1 = f(0, 0, nums, dp, n);
+        
+        return max (pickedHouse1, notPickedHouse1); 
     }
 };
