2949a260ea8cb54763f436ef39001a0b2b08ba5c
Ujjwal Chitransh <73752812+ujjwal4384@users.noreply.github.com>
2026-05-27 13:17:03 +0530

    Time: 0 ms (100%), Space: 11.7 MB (82.87%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,55 +1,40 @@
 class Solution {
-    double dfs(string node, string dst, unordered_map<string, unordered_map<string, double>>&adj, unordered_map<string, bool>&vis){
-        vis[node] = true;
-        if(node == dst) return 1.0;
-        for(auto nb: adj[node]){
-               string newNode = nb.first;
-               double cost = nb.second;
-               if(!vis[newNode]){
-                   double pathCost = dfs(newNode, dst, adj, vis);
-                   if(pathCost != -1) return cost * pathCost;
-               }
-        }
-        return -1.0;
+private:
+double dfs(string node, string dst, unordered_map<string, unordered_map<string,double>>&adj, unordered_map<string,bool>&vis){
+    if(node == dst)return 1.0;
+    vis[node] = true;
+    for(auto& p: adj[node]){   
+              string v = p.first;
+              double cost = p.second;
+              if(!vis[v]){
+                    double pathCost = dfs(v, dst, adj, vis);
+                    if(pathCost != -1){
+                        return pathCost * cost;
+                    }   
+              } 
     }
-
+   return -1; 
+}    
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        //adj: vector<vector<int>>adj;
-        unordered_map<string, unordered_map<string, double>>dis;
-        int n = equations.size();
-        set<string>nodes;
-        for(int i=0;i<n; i++){
-             string u = equations[i][0], v = equations[i][1];
-             double w = values[i];
-             dis[u][v] = w;
-             dis[v][u] = 1.0/w;
-             dis[u][u] = 1.0;
-             dis[v][v] = 1.0;
-             nodes.insert(u);
-             nodes.insert(v);
+        unordered_map<string, unordered_map<string,double>>adj;
+        for(int i=0;i<equations.size(); i++){
+            string u = equations[i][0], v = equations[i][1];
+            double val = values[i];
+            adj[u][v] = val;
+            adj[v][u] = 1.0/val;
         }
-        //FLOYD WARSAHL for precomuting V^3. and then giving in O(1) queries. 
-        for(auto& via: nodes){
-            for(auto& i: nodes){
-                for(auto& j:nodes){
-                       if(dis[i].find(j) != dis[i].end())continue;
-                       if(dis[i].find(via) != dis[i].end() && dis[via].find(j)!= dis[via].end()){
-                          dis[i][j] = dis[i][via] * dis[via][j] ;
-                       }
-                }
+        vector<double>ans;
+        for(auto& q: queries){
+            string u = q[0], v= q[1];
+            if(adj.find(u) == adj.end() || adj.find(v)== adj.end()){
+                ans.push_back(-1);
+                continue;
             }
+            unordered_map<string,bool>vis;
+            double cost = dfs(u, v, adj, vis);
+            ans.push_back(cost);
         }
-        int q = queries.size();
-        vector<double>results;
-        for(int i=0;i<q;i++){
-             auto u = queries[i][0], v = queries[i][1];
-             if(dis.find(u) == dis.end() || dis[u].find(v) == dis[u].end()){
-                results.push_back(-1);
-             }else{
-                results.push_back(dis[u][v]);
-             }
-        }
-        return results;
+       return ans; 
     }
 };
